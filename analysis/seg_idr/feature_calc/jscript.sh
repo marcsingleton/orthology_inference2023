f85db1d4318ea2e759c23415dc3ac406127dda2b
Marc Singleton <marcsingleton@berkeley.edu>
2019-08-22 16:42:43 -0700

    Added run_feature_calc.py and jscript.sh to seg_idr feature_calc folder

---
+++ analysis/seg_idr/feature_calc/jscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Key parameters
+#SBATCH --account=fc_eisenlab
+#SBATCH --partition=savio2
+#SBATCH --time=18:00:00
+#SBATCH --qos=savio_normal
+#
+# Process parameters
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --cpus-per-task=1
+#
+# Reporting parameters
+#SBATCH --job-name=feature_calc
+#SBATCH --output=feature_calc.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=marcsingleton@berkeley.edu
+#
+# Command(s) to run:
+module load python
+python feature_calc.py
---
+++ analysis/seg_idr/feature_calc/run_feature_calc.py
@@ -0,0 +1,12 @@
+"""Execute the feature_calc.py script using the unshuffled sequences as input."""
+
+from subprocess import run
+
+run('python ../../../src/feature_calc/feature_calc.py ../segment_iupred2a/segment_iupred2a.tsv ordered ord dis', shell=True)
+
+"""
+DEPENDENCIES
+../../../src/feature_calc/feature_calc.py
+../segment_iupred2a/segment_iupred2a.py
+    ../segment_iupred2a/segment_iupred2a.tsv
+"""
