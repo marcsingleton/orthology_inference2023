09c3fb4f285b92a3f3aaa00edfd3b1006ef8742a
Marc Singleton <marcsingleton@berkeley.edu>
2019-08-28 09:39:02 -0700

    Added notes to run_feature_variance.py in seg_condiv

--- analysis/seg_condiv/feature_variance/run_feature_variance.py
+++ analysis/seg_condiv/feature_variance/run_feature_variance.py
@@ -5,6 +5,10 @@ from subprocess import run
 run('python ../../../src/feature_variance.py ../feature_calc/', shell=True)
 
 """
+NOTES
+As the cutoff increases, net_charge, net_charge_P, and SCD increase as a proportion of overall variance
+    Unlike other features which are intrinsically length normalized or restricted to a certain range, these features can scale arbitrarily with length.
+
 DEPENDENCIES
 ../../../src/feature_variance.py
 ../feature_calc/run_feature_calc.py
