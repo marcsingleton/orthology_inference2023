cac6a4c337c8188db854ccff0872c606602d0b11
Marc Singleton <marcsingleton@berkeley.edu>
2019-08-28 20:56:32 -0700

    Updated notes for run_tsne_separate.py in seg_condiv

--- analysis/seg_condiv/feature_plot/run_tsne_separate.py
+++ analysis/seg_condiv/feature_plot/run_tsne_separate.py
@@ -7,28 +7,28 @@ run('python ../../../src/feature_plot_scripts/tsne_separate.py ../feature_calc/ 
 """
 NOTES
 Projecting the subsequences separately was successful in accentuating their substructures.
-    This is consistent with the commentary on nested reductions in the PCA analysis, Clusters were apparent in the combined analysis, and these clusters were a small fraction of the overall data.
+    This is consistent with the commentary on nested reductions in the PCA analysis; clusters were apparent in the combined analysis, and these clusters were a small fraction of the overall data.
 ALL
-    Charge dominates the structure of the conserved projection (as a result of their unscaled length).
-    Strands are also apparent in the diverged projections at the highest perplexities.
-        The diverged subsequences, however, also form discrete clusters, unlike the conserved subsequences.
-LEN
-    Largely similar to the all feature set, as noted previously.
-NET_CHARGE
-    The conserved projections are largely diffuse blobs with a few smaller clusters corresponding to different values of kappa and omega.
-    The diverged projections still form clusters, but they are more diffuse than those in the previous projections.
-LEN+NET_CHARGE
-    The clusters, particularly, those in the diverged projections are slightly more punctate.
-    Elongation appears to result from feature distributions with long tails that characterize multiple clusters.
-        If every cluster has a long-tailed distribution for some feature, all those clusters will attract each other which will smear their boundaries.
-        The tail for the charge distribution in the conserved subsequences is so long, all the clusters are strongly attracted to a single point.
+    Charge dominates the structure of the conserved projection (as a result of their correlation with unscaled sequence length).
+    At low cutoffs, the diverged subsequences form discrete clusters at all but the highest perplexities.
+        It is unclear if these clusters are biologically relevant.
+    At high cutoffs, these clusters merge into banded fan-like structures.
+        They are similar, but slightly less structured, than the fans in the conserved subsequences.
+MINMAX
+    The patterns in structure by cutoff are largely the same as the norm feature set.
+    The only significant difference is the central cluster in the conserved subsequences appears to have two lobes at all cutoffs (though it is more apparent at higher cutoffs).
+        Two lobes are also visible in the central cluster of the diverged subsequence projections at the highest cutoffs.
 NORM
-    Like in the combined projections, there are three major clusters corresponding to values of kappa and omega.
+    At low to medium cutoffs, there are three major clusters corresponding to values of kappa and omega like in the combined projections.
         In the conserved subsequences, these clusters are fairly uniform whereas in the diverged subsequences they display some substructure.
+    At the highest cutoffs, undefined kappa and omega clusters are nearly absent in the conserved subsequences and dramatically reduced in size in the diverged subsequences.
+        The central clusters are highly uniform, but both the conserved and diverged subsequences retain a small degree of structure.
 ZNORM
     Its star-like topology is broadly similar to the combined projection.
-        However, the conserved projections are more diffuse with little to no boundaries between the central and peripherary clusters; additionally, the points are more subtle.
-        In contrast, the diverged projections are more punctate along with the periphery and more clearly resolve the periphery clusters.
+        The conserved projections are more diffuse with little to no boundaries between the central and peripherary clusters; additionally, the points are more subtle.
+        The diverged projections are more punctate along with the periphery and more clearly resolve the periphery clusters.
+    As the length cutoffs increase, the periphery clusters decrease in size, disappearing at moderate values for the conserved subsequence projections.
+        Additionally, the diverged projections are less punctate and more diffuse, similar to the conserved projections.            
 
 DEPENDENCIES
 ../../../src/feature_plot_scripts/tsne_separate.py
