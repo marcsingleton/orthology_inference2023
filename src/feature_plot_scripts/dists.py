66391deaf5bb55f426bb157f560aa4c31d48a031
Marc Singleton <marcsingleton@berkeley.edu>
2019-09-04 14:42:53 -0700

    Updated dists.py to not plot distributions for different normalizations and to plot classes separately

--- src/feature_plot_scripts/dists.py
+++ src/feature_plot_scripts/dists.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import re
-from shared import fsets
 from sys import argv
 
 # Input variables
@@ -24,29 +23,44 @@ for path in paths:
     j1 = path.find('.tsv')
     i = int(path[j0+1:j1])
 
-    for key in ['all', 'minmax', 'znorm']:
-        # Calculate features
-        feat_all = fsets[key](df)
+    # Make output directories for cutoffs and feature sets
+    cur_dir = f'dists/{i}/'
+    if not os.path.exists(cur_dir):
+        os.makedirs(cur_dir)  # Recursive folder creation
 
-        # Make output directories for cutoffs and feature sets
-        cur_dir = f'dists/{i}/{key}/'
-        if not os.path.exists(cur_dir):
-            os.makedirs(cur_dir)  # Recursive folder creation
+    # Plot distributions
+    for feature in df.columns:
+        # Plot classes as one series
+        plt.figure()
+        _, bins, _ = plt.hist(df[feature], bins=25)
+        plt.xlabel(feature)
+        plt.ylabel('Count')
+        plt.savefig(cur_dir + f'{feature}_joint.png')
+        plt.close()
 
-        # Plot distributions
-        for feature in feat_all.columns:
-            plt.figure()
-            _, bins, _ = plt.hist(feat_all[feature], bins=25)
-            plt.xlabel(feature)
-            plt.ylabel('Count')
-            plt.savefig(cur_dir + f'dist_{feature}_joint.png')
-            plt.close()
+        # Plot classes as two series
+        plt.figure()
+        plt.hist(df.loc[T_idx][feature], label=T_name, bins=bins, alpha=0.5)
+        plt.hist(df.loc[F_idx][feature], label=F_name, bins=bins, alpha=0.5)
+        plt.xlabel(feature)
+        plt.ylabel('Count')
+        plt.legend()
+        plt.savefig(cur_dir + f'{feature}_separate.png')
+        plt.close()
 
-            plt.figure()
-            plt.hist(feat_all.loc[T_idx][feature], label=T_name, bins=bins, alpha=0.5)
-            plt.hist(feat_all.loc[F_idx][feature], label=F_name, bins=bins, alpha=0.5)
-            plt.xlabel(feature)
-            plt.ylabel('Count')
-            plt.legend()
-            plt.savefig(cur_dir + f'dist_{feature}_separate.png')
-            plt.close()
+        # Plot classes as separate graphs
+        plt.figure()
+        plt.hist(df.loc[T_idx][feature], label=T_name, bins=25, color='C0')
+        plt.xlabel(feature)
+        plt.ylabel('Count')
+        plt.legend()
+        plt.savefig(cur_dir + f'{feature}_{T_idx}.png')
+        plt.close()
+
+        plt.figure()
+        plt.hist(df.loc[F_idx][feature], label=F_name, bins=25, color='C1')
+        plt.xlabel(feature)
+        plt.ylabel('Count')
+        plt.legend()
+        plt.savefig(cur_dir + f'{feature}_{F_idx}.png')
+        plt.close()
