6ce221db004e69e7c4f47bcbee52510ee2eca14e
Marc Singleton <marcsingleton@berkeley.edu>
2019-09-05 22:31:06 -0700

    Updated notes for feature_plot/run_pca_joint.py in seg_idr

--- analysis/seg_idr/feature_plot/run_pca_joint.py
+++ analysis/seg_idr/feature_plot/run_pca_joint.py
@@ -6,6 +6,19 @@ run('python ../../../src/feature_plot_scripts/pca_joint.py ../feature_calc/ ord 
 
 """
 NOTES
+ALL
+    Across all length cutoffs, the projections have the parabolic shape observed for the longer cutoffs of the condiv subsequences.
+MINMAX
+    At cutoffs >= 16, the kappa-omega clusters are not observed.
+    The subsequences do not separate into classes as observed for the norm feature set.
+NORM
+    At all but the high length cutoff, the projections have the characteristic kappa-omega clusters.
+    At the highest length cutoff, the subsequences clearly separate into ordered and disordered classes.
+        This is potentially an effect of the decreased number of kappa-omega sequences rather than a sudden increase in the differences between the ordered and disordered classes.
+ZNORM
+    The lower length cutoff projections have fewer spokes than their counterparts in the condiv subsequences.
+        This is potentially a result of the average longer lengths of the subsequences in this set reducing the number of mono, di, and tri peptides.
+    At length cutoffs >= 16, the two classes begin to clearly separate.
 
 DEPENDENCIES
 ../../../src/feature_plot_scripts/pca_joint.py
