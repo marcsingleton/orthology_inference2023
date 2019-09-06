0d05da219c64663bdd5374c0aad0a5a5618c1aab
Marc Singleton <marcsingleton@berkeley.edu>
2019-09-05 22:41:11 -0700

    Updated notes for feature_plot/run_pca_separate.py in seg_idr

--- analysis/seg_idr/feature_plot/run_pca_separate.py
+++ analysis/seg_idr/feature_plot/run_pca_separate.py
@@ -6,6 +6,18 @@ run('python ../../../src/feature_plot_scripts/pca_separate.py ../feature_calc/ o
 
 """
 NOTES
+ALL
+    The ordered sequences generally have a fan shape while the disordered sequences have a parabolic shape.
+        The parabolic shape is likely an artifact of the SCD feature, and largely driven by a few outliers in the disordered class which inflate the variance.
+        Often projections excluding the most variable feature have a fan shape. In these cases we are likely viewing largely the same distribution along a different axis.
+MINMAX
+    In both classes, the kappa-omega clusters are visible at low to moderate cutoffs, but they are more "smeared" as compared to the norm feature set.
+        They largely disappear at higher cutoffs.
+NORM
+    The kappa-omega clusters are visible in both classes. While smaller at larger cutoffs, they do not disappear completely.
+ZNORM
+    As noted in run_pca_joint.py, the lower length cutoffs have fewer spokes compared to the condiv projections.
+    Both classes largely have no structure and the projections do not vary greatly with length cutoff.
 
 DEPENDENCIES
 ../../../src/feature_plot_scripts/pca_separate.py
