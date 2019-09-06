67ebc5c00cbe1bc2e567db3ef3d785fc55edcee9
Marc Singleton <marcsingleton@berkeley.edu>
2019-09-05 23:30:32 -0700

    Updated notes for feature_plot/run_tsne_separate.py in seg_idr

--- analysis/seg_idr/feature_plot/run_tsne_separate.py
+++ analysis/seg_idr/feature_plot/run_tsne_separate.py
@@ -6,6 +6,18 @@ run('python ../../../src/feature_plot_scripts/tsne_separate.py ../feature_calc/ 
 
 """
 NOTES
+ALL
+    The same banded fan-like structures as noted previously are observed.
+        The bands are more distinct for the ordered subsequences.
+MINMAX
+    The same pattern as noted in the joint projections is observed except the central cluster is not composed of a gradient of the ordered and disordered subsequences.
+        This reduces the number of lobes from four to two.
+    The two lobes in the ordered sequences are more closely joined at the "open" end, creating a near loop. In contrast, the lobes in the disordered sequences are open and spread apart.
+NORM
+    The same pattern as noted in the joint projections is observed except the central cluster is not composed of a gradient of the ordered and disordered subsequences.
+    Very little substructure is observed at any cutoff.
+ZNORM
+    The projections are nearly identical to their joint counterparts, excluding the separation of the central cluster into subclusters of the two classes.
 
 DEPENDENCIES
 ../../../src/feature_plot_scripts/tsne_separate.py
