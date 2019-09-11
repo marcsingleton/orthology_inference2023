d03ec3c762785c6647a8a19e9f95465ba4af86c4
Marc Singleton <marcsingleton@berkeley.edu>
2019-09-11 14:12:04 -0700

    Updated notes for feature_plot_shuffle/run_tsne_separate.py in seg_idr

--- analysis/seg_idr/feature_plot_shuffle/run_tsne_separate.py
+++ analysis/seg_idr/feature_plot_shuffle/run_tsne_separate.py
@@ -6,6 +6,20 @@ run('python ../../../src/feature_plot_scripts/tsne_separate.py ../feature_calc_s
 
 """
 NOTES
+ALL
+    The projections are generally similar. However at high length cutoffs, the fan-like structure is less uniform and more clustered in the shuffled sequences.
+        The subclusters are still arranged in a fan-like structure however.
+        The subclusters are stronger in the disordered subsequences than in the order subsequences.
+MINMAX
+    The projections are largely identical at all length cutoffs.
+        There is generally slightly more separation between the clusters in the shuffled sequences.
+        Generally, it is difficult to determine if the shuffled projections are more uniform.
+NORM
+    The shuffled projections are more uniform and have fewer nodes than do the unshuffled projections.
+        This effect is strongest at high length cutoffs but still noticeable at short length cutoffs.
+ZNORM
+    The shuffled projections are largely identical for both ordered and disordered subsequences.
+        At high cutoffs and high perplexities, a band is visible in the shuffled projections that is absent from the observed projections.
 
 DEPENDENCIES
 ../../../src/feature_plot_scripts/tsne_separate.py
