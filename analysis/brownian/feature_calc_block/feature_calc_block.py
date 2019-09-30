5d80a496b9958667bc20d7c0ad36cc8c0046def3
Marc Singleton <marcsingleton@berkeley.edu>
2019-09-30 14:38:47 -0700

    Added brownian/feature_calc_block

---
+++ analysis/brownian/feature_calc_block/feature_calc_block.py
@@ -0,0 +1,36 @@
+"""Calculate features for different length cutoffs while maintaining block identity."""
+
+import multiprocessing as mp
+import pandas as pd
+import seqfeat
+from os import environ
+
+# Input variables
+path = '../segment_avg/segment_avg.tsv'  # Path to segmented sequences
+key = 'ordered'  # Key of column denoting block class
+T_idx = 'ord'  # Index of True class
+F_idx = 'dis'  # Index of False class
+num_processes = int(environ['SLURM_NTASKS'])
+
+if __name__ == '__main__':  # Multiprocessing can only occur in top-level script (likely to prevent recursion)
+    df = pd.read_csv(path, sep='\t', keep_default_na=False)
+
+    for i in [2 ** x for x in range(6)]:
+        # Load data and subset
+        blocks_seq = []
+        blocks_id = []
+        blocks_class = []
+        for _, block in df.groupby('block_id'):
+            seqs = block['seq'].map(lambda x: x.translate({ord('-'): None}))
+            if seqs.map(lambda x: len(x) >= i).all():  # All subsequences in block must meet length cutoff
+                blocks_seq.extend(seqs)
+                blocks_id.extend(block['block_id'])
+                blocks_class.extend(block[key])
+
+        # Compute features
+        with mp.Pool(processes=num_processes) as pool:
+            results = pd.DataFrame(pool.imap(seqfeat.feat_all, blocks_seq, chunksize=50))
+
+        # Merge subsets and save
+        results.set_index([blocks_id, blocks_class], inplace=True)
+        results.to_csv(f'features_{i}.tsv', sep='\t')
