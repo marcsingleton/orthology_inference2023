f3be7e8edd1cac468a0db5814a49545c81773933
Marc Singleton <marcsingleton@berkeley.edu>
2019-10-17 22:14:33 -0700

    Updated feature_calc.py to calculate features for all non-empty sequences

--- src/feature_calc_scripts/feature_calc.py
+++ src/feature_calc_scripts/feature_calc.py
@@ -8,26 +8,23 @@ from sys import argv
 
 # Input variables
 path = argv[1]  # Path to segmented sequences
-key = argv[2]  # Key of column denoting subsequences class
+type_name = argv[2]  # Name of column denoting segment type
 num_processes = int(environ['SLURM_NTASKS'])
 
 if __name__ == '__main__':  # Multiprocessing can only occur in top-level script (likely to prevent recursion)
     segs = pd.read_csv(path, sep='\t', keep_default_na=False)
+    segs['seq'] = segs['seq'].map(lambda x: x.translate({ord('-'): None}))
+    subs_lt = segs[segs['seq'].map(lambda x: len(x) >= 1)]  # Select entries where sequence is non-empty
 
-    for i in [2 ** x for x in range(6)]:
-        # Load data and subset
-        segs_lt = segs[segs['seq'].map(lambda x: len(x) >= i)]  # Select entries where sequence is above length threshold
-        sample_T = segs_lt[segs_lt[key] == True].sample(8500)
-        sample_F = segs_lt[segs_lt[key] == False].sample(8500)
-        sample = pd.concat([sample_T, sample_F])
-        sample.to_csv(f'sequences_{i}.tsv', sep='\t')
+    # Extract indices
+    seg_ids = subs_lt['seg_id']
+    types = subs_lt[type_name]
+    lengths = subs_lt['seq'].map(len).rename('length')
 
-        # Compute features
-        with mp.Pool(processes=num_processes) as pool:
-            features_T = pd.DataFrame(pool.imap(seqfeat.feat_all, sample_T['seq'], chunksize=50))
-            features_F = pd.DataFrame(pool.imap(seqfeat.feat_all, sample_F['seq'], chunksize=50))
+    # Compute features
+    with mp.Pool(processes=num_processes) as pool:
+        results = pd.DataFrame(pool.imap(seqfeat.feat_all, subs_lt['seq'], chunksize=50))
 
-        # Merge subsets and save
-        features = pd.concat([features_T, features_F])
-        features.set_index(sample[key], inplace=True)
-        features.to_csv(f'features_{i}.tsv', sep='\t')
+    # Merge subsets and save
+    results.set_index([seg_ids, types, lengths], inplace=True)
+    results.to_csv('features.tsv', sep='\t')
