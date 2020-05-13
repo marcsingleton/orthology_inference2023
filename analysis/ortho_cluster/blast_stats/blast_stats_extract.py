759f5d93fcf1912b9fd4c80ffb3fc22d8d7c8261
Marc <marc@josephs-macbook.lan>
2020-05-13 12:29:51 -0700

    Added blast_stats/ to ortho_cluster/

---
+++ analysis/ortho_cluster/blast_stats/blast_stats_extract.py
@@ -0,0 +1,54 @@
+"""Convert BLAST best hit graph into tabular format."""
+
+import json
+import os
+import pandas as pd
+
+# Load gn metadata
+gnid2spid = {}
+with open('../ppid2meta/out/ppid2meta.tsv') as infile:
+    for line in infile:
+        _, gnid, spid = line.split()
+        gnid2spid[gnid] = spid
+
+# Load best hits graph
+with open('../blast2ggraph/out/ggraph.json') as file:
+    ggraph = json.load(file)
+
+# Extract data from graph
+records = []
+for qgnid, sgnids in ggraph.items():
+    # Remove None first to not loop over
+    if 'null' in sgnids:
+        del sgnids['null']
+    qspid = gnid2spid[qgnid]
+
+    for sgnid, qppids in sgnids.items():
+        try:
+            reciprocal = qgnid in ggraph[sgnid]
+        except KeyError:
+            reciprocal = False
+        sspid = gnid2spid[sgnid]
+
+        for qppid, sppids in qppids.items():
+            for sppid, params in sppids.items():
+                record = {'qppid': qppid, 'qgnid': qgnid, 'qspid': qspid,
+                          'sppid': sppid, 'sgnid': sgnid, 'sspid': sspid,
+                          'reciprocal': reciprocal, **params}
+                records.append(record)
+
+# Make output directory
+if not os.path.exists(f'out/'):
+    os.mkdir(f'out/')
+
+# Save as tsv
+df = pd.DataFrame(records)  # Use df to ensure columns are aligned
+df.to_csv('out/ggraph.tsv', sep='\t', index=False)
+
+"""
+DEPENDENCIES
+../blast2ggraph/blast2ggraph.py
+    ../blast2ggraph/out/ggraph.json
+../ppid2meta/ppid2meta.py
+    ../ppid2meta/out/ppid2meta.tsv
+"""
