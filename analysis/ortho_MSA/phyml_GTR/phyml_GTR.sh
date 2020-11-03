7acc085f204a7a052d32ac41c82b34f0944a42a9
Marc Singleton <marcsingleton@berkeley.edu>
2020-11-02 19:03:47 -0800

    Added phyml_GTR.sh

---
+++ analysis/ortho_MSA/phyml_GTR/phyml_GTR.sh
@@ -0,0 +1,8 @@
+# Run PhyML on meta alignments using GTR model
+
+if [ ! -d out/${1}/ ]; then
+  mkdir -p out/${1}/
+fi
+
+../../../bin/phyml -i ../make_meta/out/${1}/${2}.phy --model GTR --pinv ${3} -o tlr --search SPR --rand_start --n_rand_starts 3 > out/${1}/${2}.out
+mv ../make_meta/out/${1}/${2}.phy_phyml_rand_trees.txt ../make_meta/out/${1}/${2}.phy_phyml_stats.txt ../make_meta/out/${1}/${2}.phy_phyml_tree.txt out/${1}/
