f720cda6887ead71861ad97e755dd950c47211c4
Marc Singleton <marcsingleton@berkeley.edu>
2020-11-30 12:25:30 -0800

    Updated cluster_seqs_calc.py to use cd-hit

--- analysis/ortho_MSA/cluster_seqs/cluster_seqs_calc.py
+++ analysis/ortho_MSA/cluster_seqs/cluster_seqs_calc.py
@@ -2,31 +2,10 @@
 
 import os
 import re
-
-from Bio import pairwise2
-from Bio.Align import substitution_matrices
-
-
-def get_pident(alignment):
-    aligned1, aligned2 = alignment[:2]
-    num_sym = 0
-    num_ident = 0
-    for sym1, sym2 in zip(aligned1, aligned2):
-        num_sym += 1
-        if sym1 == sym2:
-            num_ident += 1
-    return 100 * num_ident / num_sym
-
+from subprocess import run
 
 pp_regex = {'FlyBase': r'(FBpp[0-9]+)',
             'NCBI': r'([NXY]P_[0-9]+)'}
-blosum62 = substitution_matrices.load('blosum62')
-for key, value in list(blosum62.items()):  # Necessary to prevent RunTimeError due to dictionary size change
-    if 'C' in key:
-        idx = key.index('C')
-        sym = key[(idx+1) % 2]
-        blosum62[('U', sym)] = value
-        blosum62[(sym, 'U')] = value
 
 # Parse parameters
 params = []
@@ -73,34 +52,64 @@ for _, source, prot_path in params:
                 gnid2seqs[gnid0] = [(ppid0, seq0)]
                 repr2cons[ppid0] = []
 
-# Cluster sequences
-clusters = []
-for gnid, seqs in list(gnid2seqs.items()):
-    seqs = sorted(seqs, key=lambda x: (len(x[1]), x))  # Sort by length then sequence
-    gene_clusters = []
-    for ppid0, seq0 in seqs:
-        pidents = []
-        for idx, (ppid1, _, seq1, _) in enumerate(gene_clusters):
-            alignments = pairwise2.align.globalds(seq0, seq1, blosum62, -11, -1)
-            pident = max([get_pident(alignment) for alignment in alignments])  # Get traceback with best pident
-            pidents.append((idx, pident))
-        idx, pident = max(pidents, key=lambda x: x[1]) if pidents else (0, 0)
-        if pident > 90:
-            cluster_ppids = gene_clusters[idx][3]
-            cluster_ppids.append((ppid0, pident))
-        else:
-            gene_clusters.append((ppid0, gnid, seq0, []))
-    clusters.extend(gene_clusters)
-
 # Save clusters
 if not os.path.exists('out/'):
     os.mkdir('out/')
 
+# Cluster sequences
+clusters = []
+for gnid, seqs in gnid2seqs.items():
+    if len(seqs) > 1:
+        with open(f'out/{gnid}.fasta', 'w') as file:
+            for ppid, seq in seqs:
+                file.write(f'>{ppid}\n')
+                file.write('\n'.join([seq[i:i+60] for i in range(0, len(seq), 60)]) + '\n')
+        cmd = ['../../../bin/cd-hit', '-i', f'out/{gnid}.fasta', '-o', f'out/{gnid}',
+               '-c', '0.9',  # sequence identity threshold
+               '-n', '5',  # word_length (default as recommended for high identity)
+               '-d', '0',  # full header up to first space in .clstr file description
+               '-g', '1']  # accurate mode: find best matching cluster
+        run(cmd, check=True, capture_output=True)
+
+        with open(f'out/{gnid}.clstr') as file:
+            line = file.readline()
+            while line:
+                if line.startswith('>'):
+                    line = file.readline()
+
+                cluster = []
+                while line and not line.startswith('>'):
+                    fields = line.split()
+                    ppid = fields[2][1:-3]
+                    if len(fields) == 5:
+                        pident = fields[4][:-1]
+                        cluster.append((ppid, pident))
+                    else:
+                        ppid0 = ppid
+                    line = file.readline()
+                clusters.append((ppid0, gnid, cluster))
+
+        os.remove(f'out/{gnid}.fasta')  # Original sequences
+        os.remove(f'out/{gnid}')  # Cluster sequences
+        os.remove(f'out/{gnid}.clstr')  # Clusters
+    else:
+        ppid, _ = seqs[0]
+        clusters.append((ppid, gnid, []))
+
 with open('out/clusters.tsv', 'w') as file:
     file.write('ppid\tgnid\trepr_id\tcluster_id\tpident\n')  # Identical and cluster PPIDs
-    for c_ppid, gnid, _, ppids in clusters:
-        file.write('\t'.join([c_ppid, gnid, c_ppid, c_ppid, '100']) + '\n')  # Entry for representative of cluster
-        for i_ppid, pident in ppids:
-            file.write('\t'.join([i_ppid, gnid, i_ppid, c_ppid, str(pident)]) + '\n')  # Entry for representative of identicals
-            for ppid in repr2cons[i_ppid]:
-                file.write('\t'.join([ppid, gnid, i_ppid, c_ppid, str(pident)]) + '\n')  # Entry for identical
+    for cluster_id, gnid, ppids in clusters:
+        file.write('\t'.join([cluster_id, gnid, cluster_id, cluster_id, '100.00']) + '\n')  # Entry for representative of cluster
+        for repr_id, pident in ppids:
+            file.write('\t'.join([repr_id, gnid, repr_id, cluster_id, str(pident)]) + '\n')  # Entry for representative of identicals
+            for ppid in repr2cons[repr_id]:
+                file.write('\t'.join([ppid, gnid, repr_id, cluster_id, str(pident)]) + '\n')  # Entry for identical
+
+"""
+DEPENDENCIES
+../../../data/ncbi_annotations/*/*/*/*_protein.faa
+../../../data/flybase_genomes/Drosophila_melanogaster/dmel_r6.34_FB2020_03/fasta/dmel-all-translation-r6.34.fasta
+../../ortho_cluster2/ppid2meta/ppid2meta.py
+    ../../ortho_cluster2/ppid2meta/out/ppid2meta.tsv
+./params.tsv
+"""
