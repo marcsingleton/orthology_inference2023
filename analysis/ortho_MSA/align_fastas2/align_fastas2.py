418a6548a4fbf86a383b7616774a32f26bf5688c
Marc Singleton <marcsingleton@berkeley.edu>
2020-12-07 17:04:07 -0800

    Fixed to attempt several aligners and handle exceptions

--- analysis/ortho_MSA/align_fastas2/align_fastas2.py
+++ analysis/ortho_MSA/align_fastas2/align_fastas2.py
@@ -7,22 +7,25 @@ from time import time_ns
 
 
 def run_cmd(file_id):
-    try:
-        aligner = 'probalign'
-        cmd = (f'../../../bin/probalign ../make_fastas2/out/{file_id}.tfa '
-               f'1> out/{file_id}.mfa 2> out/{file_id}.err')
-        t0 = time_ns()
-        run(cmd, shell=True, check=True)
-        t1 = time_ns()
-    except CalledProcessError:  # In rare cases probalign has overflow error which seemingly does not affect probcons
-        aligner = 'probcons'
-        cmd = (f'../../../bin/probcons ../make_fastas2/out/{file_id}.tfa '
-               f'1> out/{file_id}.mfa 2> out/{file_id}.err')
-        t0 = time_ns()
-        run(cmd, shell=True, check=True)
-        t1 = time_ns()
-
-    return file_id, aligner, str(t1-t0)
+    aligners = [('probalign', f'../../../bin/probalign ../make_fastas2/out/{file_id}.tfa '
+                              f'1> out/{file_id}.mfa 2> out/{file_id}.err'),
+                ('probcons', f'../../../bin/probcons ../make_fastas2/out/{file_id}.tfa '
+                             f'1> out/{file_id}.mfa 2> out/{file_id}.err'),
+                ('mafft', f'../../../bin/mafft --genafpair --maxiterate 1000 --thread 1 '
+                          f'../make_fastas2/out/{file_id}.tfa '
+                          f'1> out/{file_id}.mfa 2> out/{file_id}.err'),
+                ('clustalo', f'../../../bin/clustalo -i ../make_fastas2/out/{file_id}.tfa '
+                             f'--threads=1 -o out/{file_id}.mfa')]
+    for (aligner, cmd) in aligners:
+        try:
+            t0 = time_ns()
+            run(cmd, shell=True, check=True)
+            t1 = time_ns()
+            return file_id, aligner, str(t1-t0)
+        except CalledProcessError:
+            pass
+    else:
+        return file_id, 'ALIGN_ERROR', 'NaN'
 
 
 num_processes = int(os.environ['SLURM_NTASKS'])
