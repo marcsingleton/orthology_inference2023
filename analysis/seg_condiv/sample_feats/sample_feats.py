ab32b64181dbdb8366a703a0fdb4a534d8c2dc1f
Marc Singleton <marcsingleton@berkeley.edu>
2021-01-05 14:07:54 -0800

    Removed sample_feats.py

--- analysis/seg_condiv/sample_feats/sample_feats.py
+++ analysis/seg_condiv/sample_feats/sample_feats.py
@@ -3,17 +3,20 @@
 import os
 import pandas as pd
 import re
-from sys import argv
 
 # Input variables
-num_idx = int(argv[1])  # Number of index columns
-type_name = argv[2]  # Name of column denoting segment type
+num_idx = 3  # Number of index columns
+type_name = 'conserved'  # Name of column denoting segment type
 
 # Constants
 segment_dir = '../sample_segs/out/'
 features_path = '../feature_calc/out/features.tsv'
 
 features = pd.read_csv(features_path, sep='\t', index_col=list(range(num_idx)))
+
+# Make output directory
+if not os.path.exists('out/'):
+    os.mkdir('out/')
 
 paths = filter(lambda x: re.match('segments_[0-9]+\.tsv', x), os.listdir(segment_dir))
 for path in paths:
@@ -30,4 +33,12 @@ for path in paths:
     idx = tuple([slice(None) if i != num_id else segs['seg_id'] for i in range(num_idx)])
 
     sample = features.loc[idx, :].sort_index(level=[type_name, 'seg_id'], ascending=[False, True])
-    sample.to_csv(f'features_{i}.tsv', sep='\t')
+    sample.to_csv(f'out/features_{i}.tsv', sep='\t')
+
+"""
+DEPENDENCIES
+../feature_calc/feature_calc.py
+    ../feature_calc/out/features.tsv
+../sample_segs/sample_segs.py
+    ../sample_segs/out/segments_*.tsv
+"""
