3ba2cffefc81a02ccaeb53bd5c81cef680fa3792
Marc Singleton <marcsingleton@berkeley.edu>
2021-01-14 19:07:38 -0800

    Added make_ and align_fastas2-2/

---
+++ analysis/ortho_MSA/align_fastas2-2/align_fastas2-2.py
@@ -0,0 +1,40 @@
+"""Align FASTAs of remaining OGs using trees."""
+
+import multiprocessing as mp
+import os
+from subprocess import run, CalledProcessError
+from time import time_ns
+
+
+def run_cmd(file_id):
+    cmd = (f'../../../bin/clustalo -i ../make_fastas2-2/out/{file_id}.tfa '
+           f'--iterations 1 --threads=1 -o out/{file_id}.mfa')
+    try:
+        t0 = time_ns()
+        run(cmd, shell=True, check=True)
+        t1 = time_ns()
+        return file_id, str(t1-t0)
+    except CalledProcessError:
+        return file_id, 'NaN'
+
+
+num_processes = int(os.environ['SLURM_NTASKS'])
+
+if __name__ == '__main__':
+    if not os.path.exists('out/'):
+        os.mkdir('out/')
+
+    with mp.Pool(processes=num_processes) as pool:
+        file_ids = [file[:-4] for file in os.listdir('../make_fastas2-2/out/')]
+        rows = pool.map(run_cmd, file_ids)
+
+    with open('out/times.tsv', 'w') as file:
+        file.write('file_id\ttime_ns\n')
+        for row in rows:
+            file.write('\t'.join(row) + '\n')
+
+"""
+DEPENDENCIES
+../make_fastas2-2/make_fastas2-2.py
+    ../make_fastas2-2/out/*.tfa
+"""
---
+++ analysis/ortho_MSA/make_fastas2-2/make_fastas2-2.py
@@ -0,0 +1,62 @@
+"""Make FASTAs of remaining OGs using trees."""
+
+import os
+import re
+
+import pandas as pd
+
+pp_regex = {'FlyBase': r'(FBpp[0-9]+)',
+            'NCBI': r'([NXY]P_[0-9]+)'}
+
+# Parse genomes
+genomes = []
+with open('../config/genomes.tsv') as file:
+    fields = file.readline().split()  # Skip header
+    for line in file:
+        spid, _, source, prot_path = line.split()
+        genomes.append((spid, source, prot_path))
+
+# Load seqs
+ppid2seq = {}
+for spid0, source, prot_path in genomes:
+    with open(prot_path) as file:
+        line = file.readline()
+        while line:
+            if line.startswith('>'):
+                ppid = re.search(pp_regex[source], line).group(1)
+                line = file.readline()
+
+            seqlines = []
+            while line and not line.startswith('>'):
+                seqlines.append(line.rstrip())
+                line = file.readline()
+            seq = ''.join(seqlines)
+            ppid2seq[ppid] = seq
+
+# Load clusters and OG metadata
+rclusters = pd.read_table('../reduce_tree/out/rclusters.tsv').groupby('OGid')
+OGid2meta = pd.read_table('../OGid2meta/out/OGid2meta.tsv')
+
+# Write sequences
+if not os.path.exists('out/'):
+    os.mkdir('out/')
+
+OGids = OGid2meta.loc[~(OGid2meta['gnidnum'] == OGid2meta['sqidnum']), 'OGid']
+for OGid in OGids:
+    with open(f'out/{OGid}.tfa', 'w') as file:
+        for row in rclusters.get_group(OGid).itertuples():
+            seq = ppid2seq[row.ppid]
+            seqstring = '\n'.join(seq[i:i+80] for i in range(0, len(seq), 80)) + '\n'
+            file.write(f'>ppid={row.ppid}|gnid={row.gnid}|spid={row.spid}\n')
+            file.write(seqstring)
+
+"""
+DEPENDENCIES
+../../../data/ncbi_annotations/*/*/*/*_protein.faa
+../../../data/flybase_genomes/Drosophila_melanogaster/dmel_r6.34_FB2020_03/fasta/dmel-all-translation-r6.34.fasta
+../config/genomes.tsv
+../OGid2meta/OGid2meta.py
+    ../OGid2meta/out/OGid2meta.tsv
+../reduce_tree/reduce_tree.py
+    ../reduce_tree/out/rclusters.tsv
+"""
