e5e5f74ba8588a88e6cc2ca39fe62ff370b2dda8
Marc Singleton <marcsingleton@berkeley.edu>
2021-01-26 15:53:36 -0800

    Updated pOG_meta.py to calculate overlaps and bitscores

--- analysis/ortho_MSA/pOG_meta/pOG_meta.py
+++ analysis/ortho_MSA/pOG_meta/pOG_meta.py
@@ -1,6 +1,8 @@
-"""Extract metadata from pOGs including numbers of edges, genes, species, and unique sequences."""
+"""Extract metadata from pOGs including numbers of edges, genes, species, and sequences."""
 
 import os
+from itertools import groupby
+
 import pandas as pd
 
 # Load seq metadata
@@ -10,16 +12,41 @@ with open('../../ortho_search/seq_meta/out/seq_meta.tsv') as file:
         ppid, gnid, spid, _ = line.split()
         ppid2meta[ppid] = (gnid, spid)
 
+# Load pgraph
+pgraph = {}
+with open('../../ortho_cluster3/hits2pgraph/out/pgraph2.tsv') as file:
+    for line in file:
+        node, adjs = line.rstrip('\n').split('\t')
+        bitscores = {}
+        for adj in adjs.split(','):
+            adj_node, adj_bitscore = adj.split(':')
+            bitscores[adj_node] = float(adj_bitscore)
+        pgraph[node] = bitscores
+
 # Load pOGs
 rows = []
 with open('../../ortho_cluster3/clique4+_pcommunity/out/5clique/pclusters.txt') as file:
-    for line in file:
-        OGid, pOGid, edges = line.rstrip().split(':')
-        ppids = set([node for edge in edges.split('\t') for node in edge.split(',')])
-        gnids = set([ppid2meta[ppid][0] for ppid in ppids])
-        spids = set([ppid2meta[ppid][1] for ppid in ppids])
-        rows.append({'OGid': OGid, 'pOGid': pOGid,
-                     'edgenum': len(edges.split('\t')), 'ppidnum': len(ppids), 'gnidnum': len(gnids), 'spidnum': len(spids)})
+    for OGid, pOGs in groupby(file, lambda line: line.rstrip().split(':')[0]):
+        pOGid2gnids = {}  # Store gnids separately to test for overlaps
+        ds = {}  # Store rows by pOGid to modify repr after all GNIDs are collected
+        for line in pOGs:  # Calculate metadata
+            _, pOGid, edges = line.rstrip().split(':')
+            ppids = set([node for edge in edges.split('\t') for node in edge.split(',')])
+            gnids = set([ppid2meta[ppid][0] for ppid in ppids])
+            spids = set([ppid2meta[ppid][1] for ppid in ppids])
+            bitscore = 0
+            for edge in edges.split('\t'):
+                node1, node2 = edge.split(',')
+                bitscore += pgraph[node1][node2] + pgraph[node2][node1]
+
+            pOGid2gnids[pOGid] = gnids
+            ds[pOGid] = {'OGid': OGid, 'pOGid': pOGid, 'overlap': False, 'bitscore': round(bitscore, 1),
+                         'edgenum': len(edges.split('\t')), 'ppidnum': len(ppids), 'gnidnum': len(gnids), 'spidnum': len(spids)}
+        for pOGid in pOGid2gnids:  # Calculate overlap
+            gnids1 = pOGid2gnids[pOGid]
+            gnids2 = set().union(*[value for key, value in pOGid2gnids.items() if key != pOGid])
+            ds[pOGid]['overlap'] = bool(gnids1 & gnids2)
+        rows.extend(ds.values())
 pOGs = pd.DataFrame(rows)
 
 # Print counts
@@ -51,4 +78,6 @@ DEPENDENCIES
     ../../ortho_search/seq_meta/out/seq_meta.tsv
 ../../ortho_cluster3/clique4+_pcommunity/clique4+_pcommunity.py
     ../../ortho_cluster3/clique4+_pcommunity/out/5clique/pclusters.txt
+../../ortho_cluster3/hits2pgraph/hits2pgraph.py
+    ../../ortho_cluster3/hits2pgraph/out/pgraph2.tsv
 """
