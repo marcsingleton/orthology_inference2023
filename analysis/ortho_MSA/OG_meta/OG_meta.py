71b507997b1e50110c83579a63dd591828388755
Marc Singleton <marcsingleton@berkeley.edu>
2021-03-11 09:26:13 -0800

    Updated genomes in ortho_MSA/ and consolidated OG metadata scripts

--- analysis/ortho_MSA/OG_meta/OG_meta.py
+++ analysis/ortho_MSA/OG_meta/OG_meta.py
@@ -1,40 +1,91 @@
-"""Extract metadata from OGs including numbers of edges, genes, species, and unique sequences."""
+"""Extract metadata from OGs including numbers of edges, genes, species, and sequences."""
 
 import os
 import pandas as pd
 
 # Load seq metadata
-gnid2spid = {}
-gnid2sqidnum = {}
+ppid2meta = {}
 with open('../../ortho_search/seq_meta/out/seq_meta.tsv') as file:
     for line in file:
-        _, gnid, spid, repr = line.split()
-        gnid2spid[gnid] = spid
-        if repr == 'True':
-            gnid2sqidnum[gnid] = gnid2sqidnum.get(gnid, 0) + 1
+        ppid, gnid, spid, sqid = line.split()
+        ppid2meta[ppid] = (gnid, spid, sqid)
+
+# Load pgraph
+pgraph = {}
+with open('../../ortho_cluster3/hits2pgraph/out/pgraph2.tsv') as file:
+    for line in file:
+        node, adjs = line.rstrip('\n').split('\t')
+        bitscores = {}
+        for adj in adjs.split(','):
+            adj_node, adj_bitscore = adj.split(':')
+            bitscores[adj_node] = float(adj_bitscore)
+        pgraph[node] = bitscores
+
+# Load gOGs
+OGid2gOGid = {}
+with open('../../ortho_cluster3/connect_OGgraph/out/OGconnect.txt') as file:
+    for line in file:
+        gOGid, OGids = line.rstrip().split(':')
+        for OGid in OGids.split(','):
+            OGid2gOGid[OGid] = gOGid
 
 # Load OGs
 rows = []
-with open('../../ortho_cluster3/clique4+_gcommunity/out/ggraph2/5clique/gclusters.txt') as file:
+with open('../../ortho_cluster3/clique4+_pcommunity/out/pgraph2/4clique/pclusters.txt') as file:
     for line in file:
         CCid, OGid, edges = line.rstrip().split(':')
-        gnids = set([node for edge in edges.split('\t') for node in edge.split(',')])
-        spids = set([gnid2spid[gnid] for gnid in gnids])
-        sqidnum = sum([gnid2sqidnum[gnid] for gnid in gnids])
-        rows.append({'CCid': CCid, 'OGid': OGid,
-                     'edgenum': len(edges.split('\t')), 'gnidnum': len(gnids), 'spidnum': len(spids), 'sqidnum': sqidnum})
+        ppids = set([node for edge in edges.split('\t') for node in edge.split(',')])
+        gnids = set([ppid2meta[ppid][0] for ppid in ppids])
+        spids = set([ppid2meta[ppid][1] for ppid in ppids])
+        sqids = set([ppid2meta[ppid][2] for ppid in ppids])
+        bitscore = 0
+        for edge in edges.split('\t'):
+            node1, node2 = edge.split(',')
+            bitscore += pgraph[node1][node2] + pgraph[node2][node1]
+
+        d = {'CCid': CCid, 'OGid': OGid, 'gOGid': OGid2gOGid[OGid],
+             'bitscore': round(bitscore, 1), 'edgenum': len(edges.split('\t')),
+             'ppidnum': len(ppids), 'sqidnum': len(sqids),
+             'gnidnum': len(gnids), 'spidnum': len(spids)}
+        rows.append(d)
 OGs = pd.DataFrame(rows)
 
+# Find representatives
+idxs = []
+for _, group in OGs.groupby('gOGid'):
+    idx = max(group.itertuples(), key=lambda x: (x.spidnum, -x.gnidnum, x.bitscore)).Index  # Most species, least genes, highest bitscore
+    idxs.append(idx)
+OGs['repr'] = False
+OGs.loc[idxs, 'repr'] = True
+repr_OGs = OGs[OGs['repr']]
+
 # Print counts
-num = 26
+num = 31
+
+ppnum = OGs['ppidnum'] == num
+sqnum = OGs['sqidnum'] == num
 gnnum = OGs['gnidnum'] == num
 spnum = OGs['spidnum'] == num
-sqnum = OGs['sqidnum'] == num
 
+print('ALL OGS')
 print('Total OGs:', len(OGs))
 print(f'OGs with {num} genes:', len(OGs[gnnum]))
 print(f'OGs with {num} genes and species:', len(OGs[gnnum & spnum]))
-print(f'OGs with {num} genes, species, and sequences:', len(OGs[gnnum & spnum & sqnum]))
+print(f'OGs with {num} genes, species, and sequences:', len(OGs[gnnum & spnum & ppnum]))
+print(f'OGs with {num} genes, species, and unique sequences:', len(OGs[gnnum & spnum & sqnum]))
+
+ppnum = repr_OGs['ppidnum'] == num
+sqnum = repr_OGs['sqidnum'] == num
+gnnum = repr_OGs['gnidnum'] == num
+spnum = repr_OGs['spidnum'] == num
+
+print()
+print('REPRESENTATIVE OGS')
+print('Total OGs:', len(repr_OGs))
+print(f'Representative OGs with {num} genes:', len(repr_OGs[gnnum]))
+print(f'Representative OGs with {num} genes and species:', len(repr_OGs[gnnum & spnum]))
+print(f'Representative OGs with {num} genes, species, and sequences:', len(repr_OGs[gnnum & spnum & ppnum]))
+print(f'Representative OGs with {num} genes, species, and unique sequences:', len(repr_OGs[gnnum & spnum & sqnum]))
 
 # Make output directory
 if not os.path.exists('out/'):
@@ -44,14 +95,27 @@ OGs.to_csv('out/OG_meta.tsv', sep='\t', index=False)
 
 """
 OUTPUT 
-Total OGs: 14533
-OGs with 26 genes: 7903
-OGs with 26 genes and species: 7733
-OGs with 26 genes, species, and sequences: 2492
+ALL OGS
+Total OGs: 23283
+OGs with 31 genes: 7703
+OGs with 31 genes and species: 7487
+OGs with 31 genes, species, and sequences: 2047
+OGs with 31 genes, species, and unique sequences: 5091
+
+REPRESENTATIVE OGS
+Total OGs: 15099
+Representative OGs with 31 genes: 7484
+Representative OGs with 31 genes and species: 7290
+Representative OGs with 31 genes, species, and sequences: 1942
+Representative OGs with 31 genes, species, and unique sequences: 4931
 
 DEPENDENCIES
 ../../ortho_search/seq_meta/seq_meta.py
     ../../ortho_search/seq_meta/out/seq_meta.tsv
-../../ortho_cluster3/clique4+_gcommunity/clique4+_gcommunity2.py
-    ../../ortho_cluster3/clique4+_gcommunity/out/ggraph2/5clique/gclusters.txt
+../../ortho_cluster3/clique4+_pcommunity/clique4+_pcommunity2.py
+    ../../ortho_cluster3/clique4+_pcommunity/out/pgraph2/4clique/pclusters.txt
+../../ortho_cluster3/connect_OGgraph/connect_OGgraph.py
+    ../../ortho_cluster3/connect_OGgraph/out/OGconnect.txt
+../../ortho_cluster3/hits2pgraph/hits2pgraph.py
+    ../../ortho_cluster3/hits2pgraph/out/pgraph2.tsv
 """
