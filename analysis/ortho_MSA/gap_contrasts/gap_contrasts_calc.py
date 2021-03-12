6c322e7969393465cb33e16427b775e788b48bd6
Marc Singleton <marcsingleton@berkeley.edu>
2021-03-12 15:29:22 -0800

    Updated gap_contrasts/ to use updated OGs
    Now uses representative OGs obtained from OG_filter/
    Since these OGs vary in the number of genes the calculation script separates the OGs which have a complete set of species from those that do not
    The bar plots and histograms in the plotting script use OGs which have the complete set of species since the contrasts can be directly compared
    The MSA images use the full set of OGs since these will eventually be used to train the trimming script

--- analysis/ortho_MSA/gap_contrasts/gap_contrasts_calc.py
+++ analysis/ortho_MSA/gap_contrasts/gap_contrasts_calc.py
@@ -53,56 +53,57 @@ def load_msa(path):
 
 # Load tree
 tree_template = skbio.read('../../ortho_tree/consensus_tree/out/100red_ni.txt', 'newick', skbio.TreeNode)
-tree_template = tree_template.shear([tip.name for tip in tree_template.tips() if tip.name != 'sleb'])
+spids = set([tip.name for tip in tree_template.tips() if tip.name != 'sleb'])
 
-# Load pOGs and extract representatives for each OG
-pOG_meta = pd.read_table('../pOG_meta/out/pOG_meta.tsv')
-
-idxs = []
-for _, group in pOG_meta.groupby(['OGid', 'pCCid']):
-    idx = max(group.itertuples(), key=lambda x: (x.spidnum, -x.gnidnum, x.bitscore)).Index  # Most species, least genes, highest bitscore
-    idxs.append(idx)
-df1 = pOG_meta.iloc[idxs]
-df2 = df1[(df1['gnidnum'] == 26) & (df1['spidnum'] == 26)]
+# Load representative OGs
+OG_filter = pd.read_table('../OG_filter/out/OG_filter.tsv')
 
 # Calculate contrasts
 if not os.path.exists('out/'):
     os.mkdir('out/')
 
+totals = []
 rows = []
-for record in df2.itertuples():
-    if record.ppidnum == record.gnidnum:
-        msa = load_msa(f'../align_fastas1/out/{record.pOGid}.mfa')
+for record in OG_filter.itertuples():
+    if record.sqidnum == record.gnidnum:
+        msa = load_msa(f'../align_fastas1/out/{record.OGid}.mfa')
     else:
-        msa = load_msa(f'../align_fastas2-2/out/{record.pOGid}.mfa')
+        msa = load_msa(f'../align_fastas2-2/out/{record.OGid}.mfa')
 
-    tree = tree_template.deepcopy()
+    tree = tree_template.deepcopy().shear(msa.keys())
     for tip in tree.tips():
         gap_vector = np.asarray([1 if sym == '-' else 0 for sym in msa[tip.name]])
         tip.value = gap_vector
     tree.length = 0  # Set root length to 0 for convenience
 
     contrasts, _, _ = get_contrasts(tree)
-    row_sums = list(np.abs(contrasts).sum(axis=1))
     gap_matrix = np.asarray([[0 if sym == '-' else 1 for sym in seq] for seq in msa.values()])
     len1 = len(msa['dmel'])  # Total length of alignment
-    len2 = (gap_matrix / 26).sum()  # Adjusted length of alignment
-    rows.append([record.pOGid, str(len1), str(len2)] + [str(row_sum) for row_sum in row_sums])
+    len2 = (gap_matrix / len(msa)).sum()  # Adjusted length of alignment
+    totals.append([record.OGid, str(len(msa)), str(len1), str(len2), str(np.abs(contrasts).sum())])
+    if len(msa) == len(spids):
+        row_sums = list(np.abs(contrasts).sum(axis=1))
+        rows.append([record.OGid, str(len1), str(len2)] + [str(row_sum) for row_sum in row_sums])
 
+with open('out/total_sums.tsv', 'w') as file:
+    header = '\t'.join(['OGid', 'gnidnum', 'len1', 'len2', 'total']) + '\n'
+    file.writelines(header)
+    for total in totals:
+        file.writelines('\t'.join(total) + '\n')
 with open('out/row_sums.tsv', 'w') as file:
-    header = '\t'.join(['pOGid', 'len1', 'len2'] + [f'c{i}' for i in range(25)]) + '\n'
+    header = '\t'.join(['OGid', 'len1', 'len2'] + [f'row{i}' for i in range(len(spids)-1)]) + '\n'
     file.writelines(header)
     for row in rows:
         file.writelines('\t'.join(row) + '\n')
 
 """
 DEPENDENCIES
-../ortho_tree/consensus_tree/consensus_tree.py
-    ../ortho_tree/consensus_tree/out/100red_ni.txt
+../../ortho_tree/consensus_tree/consensus_tree.py
+    ../../ortho_tree/consensus_tree/out/100red_ni.txt
 ../align_fastas1/align_fastas1.py
     ../align_fastas1/out/*.mfa
 ../align_fastas2-2/align_fastas2-2.py
     ../align_fastas2-2/out/*.mfa
-../pOG_meta/pOG_meta.py
-    ../pOG_meta/out/pOG_meta.tsv
+../OG_meta/OG_meta.py
+    ../OG_meta/out/OG_meta.tsv
 """
--- analysis/ortho_MSA/gap_contrasts/gap_contrasts_stats.py
+++ analysis/ortho_MSA/gap_contrasts/gap_contrasts_stats.py
@@ -29,34 +29,33 @@ def load_msa(path):
     return msa
 
 
-tree = skbio.read('../../ortho_tree/consensus_tree/out/100red_ni.txt', 'newick', skbio.TreeNode)
-tree = tree.shear([tip.name for tip in tree.tips() if tip.name != 'sleb'])
-order = {tip.name: i for i, tip in enumerate(tree.tips())}
+OG_filter = pd.read_table('../OG_filter/out/OG_filter.tsv')
+tree_template = skbio.read('../../ortho_tree/consensus_tree/out/100red_ni.txt', 'newick', skbio.TreeNode)
+spids = set([tip.name for tip in tree_template.tips() if tip.name != 'sleb'])
+num_contrasts = len(spids) - 1
 
-pOG_meta = pd.read_table('../pOG_meta/out/pOG_meta.tsv')
-df = pd.read_table('out/row_sums.tsv').merge(pOG_meta, on='pOGid', how='left')
-df['c_sum'] = df[[f'c{i}' for i in range(25)]].sum(axis=1)
-df['c_avg'] = df['c_sum'] / df['len2']
+# 1 PLOT STATISTICS (OGS WITH ALL SPECIES)
+df = pd.read_table('out/row_sums.tsv').merge(OG_filter, on='OGid', how='left')
+df['total'] = df[[f'row{i}' for i in range(num_contrasts)]].sum(axis=1)
+df['avg'] = df['total'] / df['len2']
 
-# 1 PLOT STATISTICS
 # 1.1 Tail fraction
-num_contrasts = 25
 max_percentile = 0.30
-
-size = ceil(0.015 * len(df))
+window_percent = 3
+window_width = ceil(window_percent / 200 * len(df))
 max_idx = int(max_percentile * len(df)) - 1  # Subtract 1 due to 0 indexing
-x = [i / len(df) for i in range(size + 1, max_idx - size + 1)]
+x = [i / len(df) for i in range(window_width + 1, max_idx - window_width + 1)]
 
-contrasts = pd.concat([df[f'c{i}'] for i in range(num_contrasts)], keys=[f'c{i}' for i in range(num_contrasts)],
-                      names=['contrast_id', 'pOGid']).sort_values(ascending=False)
-counts = [(i, contrasts[:max_idx].index.get_level_values('contrast_id') == f'c{i}') for i in range(num_contrasts)]
-tails = sorted([(i, np.convolve(count, np.ones(2 * size + 1) / (2 * size + 1), 'valid')) for i, count in counts],
+contrasts = pd.concat([df[f'row{i}'] for i in range(num_contrasts)], keys=[f'row{i}' for i in range(num_contrasts)],
+                      names=['contrast_id', 'OGid']).sort_values(ascending=False)
+counts = [(i, contrasts[:max_idx].index.get_level_values('contrast_id') == f'row{i}') for i in range(num_contrasts)]
+tails = sorted([(i, np.convolve(count, np.ones(2 * window_width + 1) / (2 * window_width + 1), 'valid')) for i, count in counts],
                key=lambda y: sum(y[1]), reverse=True)
 for i, tail in tails[:9]:
-    plt.plot(x, tail, label=f'c{i}', linewidth=1)
+    plt.plot(x, tail, label=i, linewidth=1)
 plt.plot(x, sum([tail for _, tail in tails[9:]]), label='others', linewidth=1)
 plt.legend()
-plt.title('Contrast Fractions in 3% Sliding Windows')
+plt.title(f'Contrast Fractions in {window_percent}% Sliding Windows')
 plt.xlabel('Right Tail Percentile')
 plt.ylabel('Fraction')
 plt.legend(title='Contrast ID', bbox_to_anchor=(1.025, 0.5), loc='center left')
@@ -74,52 +73,60 @@ plt.savefig('out/hist_contrast_log.png')
 plt.close()
 
 # 1.3 Distribution of averages within OGs
-plt.hist(contrasts.groupby('pOGid').mean(), bins=200)
-plt.xlabel('Contrast mean in pOG')
+plt.hist(contrasts.groupby('OGid').mean(), bins=200)
+plt.xlabel('Contrast mean in OG')
 plt.ylabel('Count')
-plt.savefig('out/hist_contrast_pOGmean.png')
+plt.savefig('out/hist_contrast_OGmean.png')
 plt.yscale('log')
-plt.savefig('out/hist_contrast_pOGmean_log.png')
+plt.savefig('out/hist_contrast_OGmean_log.png')
 plt.close()
 
 # 1.4 Contrast averages across all OGs
-plt.bar(list(range(num_contrasts)), [df[f'c{i}'].mean() for i in range(num_contrasts)],
-        yerr=[df[f'c{i}'].std()/50 for i in range(num_contrasts)])
+plt.bar(list(range(num_contrasts)), [df[f'row{i}'].mean() for i in range(num_contrasts)],
+        yerr=[df[f'row{i}'].std()/50 for i in range(num_contrasts)])
 plt.xlabel('Contrast ID')
 plt.ylabel('Mean ± STD/50')
 plt.savefig('out/bar_contrast_mean.png')
 plt.close()
 
-# 2 DRAW ALIGNMENTS
-if not os.path.exists('out/sum/'):
-    os.mkdir('out/sum/')
+# 2 DRAW ALIGNMENTS (ALL OGS)
+df = pd.read_table('out/total_sums.tsv').merge(OG_filter[['OGid', 'sqidnum']], on='OGid', how='left')  # total_sums.tsv has gnidnum already
+df['norm1'] = df['total'] / df['gnidnum']
+df['norm2'] = df['total'] / (df['gnidnum'] * df['len2'])
+
+if not os.path.exists('out/norm1/'):
+    os.mkdir('out/norm1/')
 
 # 2.1 Ranked by sum
-head1 = df.sort_values(by='c_sum', ascending=False).head(100)
+head1 = df.sort_values(by='norm1', ascending=False).head(150)
 for i, record in enumerate(head1.itertuples()):
-    if record.ppidnum == record.gnidnum:
-        msa = load_msa(f'../align_fastas1/out/{record.pOGid}.mfa')
+    if record.sqidnum == record.gnidnum:
+        msa = load_msa(f'../align_fastas1/out/{record.OGid}.mfa')
     else:
-        msa = load_msa(f'../align_fastas2-2/out/{record.pOGid}.mfa')
+        msa = load_msa(f'../align_fastas2-2/out/{record.OGid}.mfa')
 
+    tree = tree_template.shear([seq[0] for seq in msa])
+    order = {tip.name: i for i, tip in enumerate(tree.tips())}
     msa = [seq[1] for seq in sorted(msa, key=lambda x: order[x[0]])]  # Re-order sequences and extract seq only
     im = draw_msa(msa)
-    plt.imsave(f'out/sum/{i}_{record.pOGid}.png', im)
+    plt.imsave(f'out/norm1/{i}_{record.OGid}.png', im)
 
 # 2.2 Ranked by avg
-if not os.path.exists('out/avg/'):
-    os.mkdir('out/avg/')
+if not os.path.exists('out/norm2/'):
+    os.mkdir('out/norm2/')
 
-head1 = df.sort_values(by='c_avg', ascending=False).head(100)
+head1 = df.sort_values(by='norm2', ascending=False).head(150)
 for i, record in enumerate((head1.itertuples())):
-    if record.ppidnum == record.gnidnum:
-        msa = load_msa(f'../align_fastas1/out/{record.pOGid}.mfa')
+    if record.sqidnum == record.gnidnum:
+        msa = load_msa(f'../align_fastas1/out/{record.OGid}.mfa')
     else:
-        msa = load_msa(f'../align_fastas2-2/out/{record.pOGid}.mfa')
+        msa = load_msa(f'../align_fastas2-2/out/{record.OGid}.mfa')
 
+    tree = tree_template.shear([seq[0] for seq in msa])
+    order = {tip.name: i for i, tip in enumerate(tree.tips())}
     msa = [seq[1] for seq in sorted(msa, key=lambda x: order[x[0]])]  # Re-order sequences and extract seq only
     im = draw_msa(msa)
-    plt.imsave(f'out/avg/{i}_{record.pOGid}.png', im)
+    plt.imsave(f'out/norm2/{i}_{record.OGid}.png', im)
 
 """
 ../../../src/draw.py
@@ -129,8 +136,8 @@ for i, record in enumerate((head1.itertuples())):
     ../align_fastas1/out/*.mfa
 ../align_fastas2-2/align_fastas2-2.py
     ../align_fastas2-2/out/*.mfa
-../pOG_meta/pOG_meta.py
-    ../pOG_meta/out/pOG_meta.tsv
+../OG_meta/OG_meta.py
+    ../OG_meta/out/OG_meta.tsv
 ./gap_contrasts_calc.py
     ./out/row_sums.tsv
 """
