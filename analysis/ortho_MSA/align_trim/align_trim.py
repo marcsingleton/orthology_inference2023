8e2f7d9c49b5962ab6bb16a03c71a76a5331b983
Marc Singleton <marcsingleton@berkeley.edu>
2021-03-23 10:45:14 -0700

    Updated align_trim/ to use final versions of parameters and trim.py

--- analysis/ortho_MSA/align_trim/align_trim.py
+++ analysis/ortho_MSA/align_trim/align_trim.py
@@ -2,48 +2,60 @@
 
 import json
 import os
-import re
-from itertools import chain
 
+import pandas as pd
 import skbio
-from src.ortho_MSA.trim import constants, trim_msa
+from src.ortho_MSA.trim import trim_msa
 
-# TAXONOMIC CONDITIONS
-conditions = [(set(['dinn', 'dgri']), 1),
-              (set(['dnov', 'dvir', 'dhyd', 'dmoj', 'dnav']), 4),
-              (set(['dper', 'dpse']), 1),
-              (set(['dobs', 'dgua', 'dsub']), 2),
-              (set(['dana', 'dbip', 'dkik', 'dser']), 3),
-              (set(['dele', 'dfic', 'deug', 'dere']), 3),
-              (set(['dtak', 'dbia']), 1),
-              (set(['dmel']), 1),
-              (set(['dmau', 'dsec']), 1)]
+# Load parameters
+with open('trim_params.json') as file:
+    tp = json.load(file)  # Trim parameters
+with open('../config/trim_params.json') as file:
+    tp.update(json.load(file))
+matrix = {}
+with open('../config/BLOSUM62.txt') as file:
+    file.readline()  # Skip header
+    syms = file.readline().split()
+    for i, line in enumerate(file):
+        for j, value in enumerate(line.split()[1:]):
+            matrix[(syms[i], syms[j])] = int(value)
 
-with open('../config/constants.json') as file:
-    config = json.load(file)
-constants.update(config)
+OG_filter = pd.read_table('../OG_filter/out/OG_filter.tsv')
 
 if not os.path.exists('out/'):
     os.mkdir('out/')
 
-paths1 = [f'../align_fastas1/out/{file}' for file in os.listdir('../align_fastas1/out/') if file.endswith('.mfa')]
-paths2 = [f'../align_fastas2-2/out/{file}' for file in os.listdir('../align_fastas2-2/out/') if file.endswith('.mfa')]
-for path in chain(paths1, paths2):
+for row in OG_filter.itertuples():
     # Load MSA
-    msa1 = skbio.read(path, format='fasta', into=skbio.TabularMSA, constructor=skbio.Protein)
-    for seq in msa1:
-        match = re.match('ppid=([A-Za-z_0-9]+)\|gnid=([A-Za-z_0-9]+)\|spid=([a-z]+)', seq.metadata['id'])
-        d = {key: value for key, value in zip(['ppid', 'gnid', 'spid'], match.groups())}
-        seq.metadata.update(d)
+    if row.sqidnum == row.gnidnum:
+        msa1 = skbio.read(f'../align_fastas1/out/{row.OGid}.mfa',
+                          format='fasta', into=skbio.TabularMSA, constructor=skbio.Protein)
+    else:
+        msa1 = skbio.read(f'../align_fastas2-2/out/{row.OGid}.mfa',
+                          format='fasta', into=skbio.TabularMSA, constructor=skbio.Protein)
 
-    # Test for species groups
-    spids = set([seq.metadata['spid'] for seq in msa1])
-    if any([len(spids & group) < num for group, num in conditions]):
-        continue
+    # Trim MSA and remove gap only columns
+    msa2 = trim_msa(msa1,
+                    tp['con_frac'], tp['con_window'], tp['con_minlen'], tp['con_rate'], tp['con_minsig'],
+                    tp['gap_num'], tp['gap_rate'], tp['gap_minsig'],
+                    tp['gp_sigma'], tp['gd_window'], tp['indel1_rate'], tp['indel2_rate'],
+                    tp['weights'], tp['threshold'],
+                    matrix)
+    slices, i = [], None
+    for j, col in enumerate(msa2.iter_positions()):
+        for sym in col:
+            if sym != '-':
+                if i is None:  # Store position only if new slice is not started
+                    i = j
+                break
+        else:
+            slices.append(slice(i, j))
+            i = None
+    if i is not None:  # Add final slice to end
+        slices.append(slice(i, msa2.shape[1]))
+    msa3 = msa2.loc[:, slices]
 
-    # Trim MSA and save
-    msa2 = trim_msa(msa1)
-    skbio.write(msa1, format='fasta', into=f'out/{path[-8:]}', max_width=80)
+    skbio.write(msa3, format='fasta', into=f'out/{row.OGid}.mfa', max_width=80)
 
 """
 DEPENDENCIES
@@ -51,4 +63,6 @@ DEPENDENCIES
     ../align_fastas1/out/*.mfa
 ../align_fastas2-2/align_fastas2-2.py
     ../align_fastas2-2/out/*.mfa
+../OG_filter/OG_filter.py
+    ../OG_filter/out/OG_filter.tsv
 """
