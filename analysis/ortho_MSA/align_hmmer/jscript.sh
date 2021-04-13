ee739422d1677fb61c3a5612b271c65ddce9441b
Marc Singleton <marcsingleton@berkeley.edu>
2021-04-12 17:44:42 -0700

    Added creation of link to scratch for output

--- analysis/ortho_MSA/align_hmmer/jscript.sh
+++ analysis/ortho_MSA/align_hmmer/jscript.sh
@@ -16,6 +16,15 @@
 #SBATCH --mail-user=marcsingleton@berkeley.edu
 #
 # Command(s) to run:
+# Set current directory and link to output in scratch
+if [ ! -d out ]; then
+  out_dir=/global/scratch/singleton/IDREvoDevo/analysis/ortho_MSA/align_hmmer/out/
+  if [ ! -d ${out_dir} ]; then
+    mkdir -p ${out_dir}  # -p makes intermediate directory if they do not exist
+  fi
+  ln -s ${out_dir} out
+fi
+
 source /global/home/users/singleton/.bashrc
 conda activate IDREvoDevo
 python align_hmmer.py
