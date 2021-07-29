95be173bb61c70f154314fd6e4dd193e41391854
Marc Singleton <marcsingleton@berkeley.edu>
2021-07-29 15:26:41 -0700

    Added aucpred_segment/

---
+++ analysis/brownian2/aucpred_segment/run.py
@@ -0,0 +1,49 @@
+"""Run AUCpreD on individual sequences in trimmed alignments."""
+
+import os
+import re
+import subprocess
+
+
+def load_msa(path):
+    msa = []
+    with open(path) as file:
+        line = file.readline()
+        while line:
+            if line.startswith('>'):
+                header = line
+                line = file.readline()
+
+            seqlines = []
+            while line and not line.startswith('>'):
+                seqlines.append(line.rstrip())
+                line = file.readline()
+            seq = ''.join(seqlines)
+            msa.append((header, seq))
+    return msa
+
+
+if not os.path.exists('out/'):
+    os.mkdir('out/')
+
+OGids = [path.split('.') for path in os.listdir('../trim_extract/out/') if path.endswith('.mfa')]
+for OGid in OGids:
+    msa = load_msa(f'../trim_extract/out/{OGid}.mfa')
+
+    if not os.path.exists(f'out/{OGid}/'):
+        os.mkdir(f'out/{OGid}/')
+
+    for header, seq in msa:
+        ppid = re.match(r'>ppid=([A-Za-z0-9_]+)\|', header).group(1)
+        with open(f'out/{OGid}/{ppid}.fasta', 'w') as file:
+            seqstring = '\n'.join([seq[i:i+80] for i in range(0, len(seq), 80)]) + '\n'
+            file.write(header + seqstring)
+        subprocess.run(f'../../../bin/Predict_Property/AUCpreD.sh -i out/{OGid}/{ppid}.fasta -o out/{OGid}/',
+                       check=True, shell=True)
+        os.remove(f'out/{OGid}/{ppid}.fasta')
+
+"""
+DEPENDENCIES
+../trim_extract/extract.py
+    ../trim_extract/out/*.mfa
+"""
