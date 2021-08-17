7465886d4c5990aaf5ded63bf6eb376420cfd6c9
Marc Singleton <marcsingleton@berkeley.edu>
2021-08-17 13:01:15 -0700

    Added get_features/

---
+++ analysis/brownian2/get_features/get_features.py
@@ -0,0 +1,85 @@
+"""Calculate features of segments in regions."""
+
+import multiprocessing as mp
+import os
+import re
+from collections import namedtuple
+
+import features
+
+
+Record = namedtuple('Record', ['OGid', 'start', 'stop', 'ppid', 'disorder', 'segment'])
+
+
+def load_msa(path):
+    msa = []
+    with open(path) as file:
+        line = file.readline()
+        while line:
+            if line.startswith('>'):
+                header = line.rstrip()
+                line = file.readline()
+
+            seqlines = []
+            while line and not line.startswith('>'):
+                seqlines.append(line.rstrip())
+                line = file.readline()
+            seq = ''.join(seqlines)
+            msa.append((header, seq))
+    return msa
+
+
+def get_features(record):
+    d = {'OGid': record.OGid, 'start': record.start, 'stop': record.stop, 'ppid': record.ppid}
+    d.update(features.get_features(record.segment))
+    return d
+
+
+num_processes = int(os.environ['SLURM_CPUS_ON_NODE'])
+ppid_regex = r'ppid=([A-Za-z0-9_]+)'
+
+if __name__ == '__main__':
+    # Load regions
+    OGid2regions = {}
+    with open('../aucpred_segment/out/segments.tsv') as file:
+        file.readline()  # Skip header
+        for line in file:
+            OGid, start, stop, disorder = line.split()
+            try:
+                OGid2regions[OGid].append((int(start), int(stop), disorder))
+            except KeyError:
+                OGid2regions[OGid] = [(int(start), int(stop), disorder)]
+
+    # Extract segments
+    args = []
+    for OGid, regions in list(OGid2regions.items()):
+        msa = load_msa(f'../trim_extract/out/{OGid}.mfa')
+        msa = {re.search(ppid_regex, header).group(1): seq for header, seq in msa}
+
+        for start, stop, disorder in regions:
+            for ppid, seq in msa.items():
+                segment = seq[start:stop].translate({ord('-'): None, ord('.'): None})
+                args.append(Record(OGid, start, stop, ppid, disorder, segment))
+
+    # Calculate features
+    with mp.Pool(processes=num_processes) as pool:
+        records = pool.map(get_features, args, chunksize=50)
+
+    # Write output
+    if not os.path.exists('out/'):
+        os.mkdir('out/')
+
+    with open('out/features.tsv', 'w') as file:
+        if records:
+            fields = list(records[0])
+            file.write('\t'.join(fields) + '\n')
+        for record in records:
+            file.write('\t'.join(str(record[field]) for field in fields) + '\n')
+
+"""
+DEPENDENCIES
+../aucpred_segment/aucpred_segment.py
+    ../aucpred_segment/out/segments.tsv
+../trim_extract/trim_extract.py
+    ../trim_extract/out/*.mfa
+"""
---
+++ analysis/brownian2/get_features/jscript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Key parameters
+#SBATCH --account=fc_eisenlab
+#SBATCH --partition=savio2
+#SBATCH --time=12:00:00
+#SBATCH --qos=savio_normal
+#
+# Process parameters
+#SBATCH --nodes=1
+#
+# Reporting parameters
+#SBATCH --job-name=get_features
+#SBATCH --output=run.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=marcsingleton@berkeley.edu
+#
+# Command(s) to run:
+# Link to output in scratch
+if [ ! -d out ]; then
+  out_dir=/global/scratch/singleton/IDREvoDevo/analysis/brownian2/get_features/out/
+  if [ ! -d ${out_dir} ]; then
+    mkdir -p ${out_dir}  # -p makes intermediate directory if they do not exist
+  fi
+  ln -s ${out_dir} out
+fi
+
+source /global/home/users/singleton/.bashrc
+conda activate IDREvoDevo
+python get_features.py
