bde43c663c5c559a0145376f5871f6a7bd7403f9
Marc Singleton <marcsingleton@berkeley.edu>
2021-08-31 15:47:39 -0700

    Added deletion_decode/

---
+++ analysis/brownian2/deletion_decode/decode.py
@@ -0,0 +1,105 @@
+"""Decode long deletions of trimmed alignments."""
+
+import json
+import multiprocessing as mp
+import os
+import re
+
+import src.hmm as hmm
+
+
+class msaBernoulli:
+    def __init__(self, ps):
+        self.ps = ps
+
+    def pmf(self, x):
+        p = self.ps[x[0]]
+        if x[1] == 1:
+            return p
+        else:
+            return 1 - p
+
+    def rvs(self, size=None):
+        # Required for HMM since it has a simulate method
+        # Simulations aren't used here, so it's an empty method
+        pass
+
+
+def load_msa(path):
+    msa = []
+    with open(path) as file:
+        line = file.readline()
+        while line:
+            if line.startswith('>'):
+                header = line.rstrip()
+                line = file.readline()
+
+            seqlines = []
+            while line and not line.startswith('>'):
+                seqlines.append(line.rstrip())
+                line = file.readline()
+            seq = ''.join(seqlines)
+            msa.append((header, seq))
+    return msa
+
+
+def decode(OGid, params):
+    # Load msa
+    msa = load_msa(f'../insertion_trim/out/{OGid}.mfa')
+
+    # Create Bernoulli sequence
+    ps = []
+    for j in range(len(msa[0][1])):
+        col = [1 if msa[i][1][j] in ['-', '.'] else 0 for i in range(len(msa))]
+        p = sum(col) / len(col)
+        ps.append(p)
+
+    # Instantiate model
+    e_dists_rv = {'0': msaBernoulli(ps), '1': msaBernoulli([params['e_param'] for _ in range(len(ps))])}
+    model = hmm.HMM(params['t_dists'], e_dists_rv, params['start_dist'])
+
+    # Decode states
+    records = []
+    for header, seq in msa:
+        # Create emission sequence
+        emits = []
+        for j, sym in enumerate(seq):
+            if sym in ['-', '.']:
+                emits.append((j, 1))
+            else:
+                emits.append((j, 0))
+
+        ppid = re.search(ppid_regex, header).group(1)
+        fbs = model.forward_backward(emits)
+        records.append((ppid, fbs))
+
+    # Write decoded states
+    with open(f'out/{OGid}.tsv', 'w') as file:
+        file.write('\t'.join(['ppid'] + states) + '\n')
+        for ppid, fbs in records:
+            for fb in zip(*[fbs[state] for state in states]):
+                file.write(ppid + '\t' + '\t'.join([str(v) for v in fb]) + '\n')
+
+
+ppid_regex = r'ppid=([A-Za-z0-9_]+)'
+num_processes = int(os.environ['SLURM_CPUS_ON_NODE'])
+states = ['0', '1']
+
+if __name__ == '__main__':
+    with open('../deletion_hmm/out/model.json') as file:
+        params = json.load(file)
+
+    if not os.path.exists('out/'):
+        os.mkdir('out/')
+
+    with mp.Pool(processes=num_processes) as pool:
+        args = [(path.split('.')[0], params) for path in os.listdir('../insertion_trim/out/') if path.endswith('.mfa')]
+        pool.starmap(decode, args)
+
+"""
+DEPENDENCIES
+../insertion_trim/extract.py
+    ../insertion_trim/out/*.mfa
+../deletion_hmm/fit.py
+    ../deletion_hmm/out/model.json
+"""
---
+++ analysis/brownian2/deletion_decode/jscript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Key parameters
+#SBATCH --account=fc_eisenlab
+#SBATCH --partition=savio
+#SBATCH --time=24:00:00
+#SBATCH --qos=savio_normal
+#
+# Process parameters
+#SBATCH --nodes=1
+#
+# Reporting parameters
+#SBATCH --job-name=decode
+#SBATCH --output=decode.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=marcsingleton@berkeley.edu
+#
+# Command(s) to run:
+# Link to output in scratch
+if [ ! -d out ]; then
+  out_dir=/global/scratch/users/singleton/IDREvoDevo/analysis/brownian2/deletion_decode/out/
+  if [ ! -d ${out_dir} ]; then
+    mkdir -p ${out_dir}  # -p makes intermediate directory if they do not exist
+  fi
+  ln -s ${out_dir} out
+fi
+
+source /global/home/users/singleton/.bashrc
+conda activate IDREvoDevo
+python decode.py
