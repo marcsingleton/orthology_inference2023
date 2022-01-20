adf18978e156371f87e9a80d332c8f4e01539246
Marc Singleton <marcsingleton@berkeley.edu>
2022-01-20 12:12:33 -0800

    Updated to use external labels for plots

--- analysis/evosim/iqtree_plot/iqtree_plot.py
+++ analysis/evosim/iqtree_plot/iqtree_plot.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+from collections import namedtuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,8 +13,9 @@ from matplotlib.patches import Circle, Ellipse
 
 order = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I',
          'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
-labels = ['0red_D', '50red_D', '100red_D',
-          '0red_O', '50red_O', '100red_O']
+labels = [('disorder_0%', '0red_D'), ('disorder_50%', '50red_D'), ('disorder_100%', '100red_D'),
+          ('order_0%', '0red_O'), ('order_50%', '50red_O'), ('order_100%', '100red_O')]
+Record = namedtuple('Record', ['plot_label', 'file_label', 'matrix', 'freqs'])
 
 # Read WAG model
 with open('../config/WAG.txt') as file:
@@ -34,8 +36,8 @@ WAG_matrix = WAG_matrix / rate  # Normalize average rate to 1
 
 # Read IQ-TREE matrices
 records = []
-for label in labels:
-    with open(f'../iqtree_fit/out/{label}.iqtree') as file:
+for plot_label, file_label in labels:
+    with open(f'../iqtree_fit/out/{file_label}.iqtree') as file:
         # Move to exchangeability matrix and parse
         line = file.readline()
         while not line.startswith('Substitution parameters'):
@@ -72,7 +74,7 @@ for label in labels:
 
     rate = (freqs * (freqs * matrix).sum(axis=1)).sum()
     matrix = matrix / rate
-    records.append((label, matrix, freqs))
+    records.append(Record(plot_label, file_label, matrix, freqs))
 
 # Make plots
 if not os.path.exists('out/'):
@@ -80,10 +82,10 @@ if not os.path.exists('out/'):
 
 # 1 HEATMAP
 fig, axs = plt.subplots(2, 3, figsize=(8, 6))
-vmax = max([record[1].max() for record in records])
-for ax, (label, matrix, _) in zip(axs.ravel(), records):
-    ax.imshow(matrix, vmax=vmax)
-    ax.set_title(label)
+vmax = max([record.matrix.max() for record in records])
+for ax, record in zip(axs.ravel(), records):
+    ax.imshow(record.matrix, vmax=vmax)
+    ax.set_title(record.plot_label)
     ax.set_xticks(range(20))
     ax.set_xticklabels(order, fontdict={'fontsize': 7})
     ax.set_yticks(range(20))
@@ -95,8 +97,8 @@ plt.close()
 # 2 DOT PLOT
 scale = 0.0325
 fig, axs = plt.subplots(2, 3, figsize=(8, 6))
-for ax, (label, matrix, _) in zip(axs.ravel(), records):
-    ax.set_title(label)
+for ax, record in zip(axs.ravel(), records):
+    ax.set_title(record.plot_label)
     ax.set_xlim(0, 20)
     ax.set_xticks(range(1, 21))
     ax.set_xticklabels(order, fontdict={'fontsize': 7})
@@ -107,7 +109,7 @@ for ax, (label, matrix, _) in zip(axs.ravel(), records):
     ax.set_axisbelow(True)
     ax.set_aspect(1)
 
-    for i, row in enumerate(matrix):
+    for i, row in enumerate(record.matrix):
         for j, value in enumerate(row):
             if j >= i:
                 continue
@@ -118,10 +120,10 @@ plt.close()
 
 # 3 DOT COMPARISONS
 scale = 0.03
-pairs = [(records[1][0], records[1][1], 'black', 'WAG', WAG_matrix, 'white'),
-         (records[4][0], records[4][1], 'grey', 'WAG', WAG_matrix, 'white'),
-         (records[1][0], records[1][1], 'black', records[4][0], records[4][1], 'grey')]
-for l1, m1, c1, l2, m2, c2 in pairs:
+pairs = [(records[1].plot_label, records[1].file_label, records[1].matrix, 'black', 'WAG', 'WAG', WAG_matrix, 'white'),
+         (records[4].plot_label, records[4].file_label, records[4].matrix, 'grey', 'WAG', 'WAG', WAG_matrix, 'white'),
+         (records[1].plot_label, records[1].file_label, records[1].matrix, 'black', records[4].plot_label, records[4].file_label, records[4].matrix, 'grey')]
+for pl1, fl1, m1, c1, pl2, fl2, m2, c2 in pairs:
     fig, ax = plt.subplots(figsize=(9, 4))
     ax.set_xlim(0, 20)
     ax.set_xticks(range(1, 21))
@@ -131,7 +133,7 @@ for l1, m1, c1, l2, m2, c2 in pairs:
     ax.set_yticklabels(order[::-1], fontdict={'fontsize': 8})
     ax.grid(True)
     ax.set_axisbelow(True)
-    ax.set_aspect(0.5)
+    ax.set_aspect(0.5)  # Scale vertical axis half of horizontal axis
 
     for i, row in enumerate(m1):
         for j, value in enumerate(row):
@@ -145,21 +147,21 @@ for l1, m1, c1, l2, m2, c2 in pairs:
                 continue
             c = Ellipse((j + 1.5, 20 - i), height=2*scale*value, width=scale*value, facecolor=c2, edgecolor='black')
             ax.add_patch(c)
-    handles = [Line2D([], [], label=l1, marker='o', markerfacecolor=c1, markeredgecolor='black', markersize=8, linestyle='None'),
-               Line2D([], [], label=l2, marker='o', markerfacecolor=c2, markeredgecolor='black', markersize=8, linestyle='None')]
+    handles = [Line2D([], [], label=pl1, marker='o', markerfacecolor=c1, markeredgecolor='black', markersize=8, linestyle='None'),
+               Line2D([], [], label=pl2, marker='o', markerfacecolor=c2, markeredgecolor='black', markersize=8, linestyle='None')]
     ax.legend(handles=handles, bbox_to_anchor=(1, 0.5), loc='center left')
 
-    plt.savefig(f'out/dotplot_adj_{l1}-{l2}.png', bbox_inches='tight')
+    plt.savefig(f'out/dotplot_adj_{fl1}-{fl2}.png', bbox_inches='tight')
     plt.close()
 
 # 4 RATIO DOT COMPARISONS
 scale = 0.15
-pairs = [(records[1][0], records[1][1], 'WAG', WAG_matrix),
-         (records[4][0], records[4][1], 'WAG', WAG_matrix),
-         (records[1][0], records[1][1], records[4][0], records[4][1])]
-for l1, m1, l2, m2 in pairs:
+pairs = [(records[1].plot_label, records[1].file_label, records[1].matrix, 'WAG', 'WAG', WAG_matrix),
+         (records[4].plot_label, records[4].file_label, records[4].matrix, 'WAG', 'WAG', WAG_matrix),
+         (records[1].plot_label, records[1].file_label, records[1].matrix, records[4].plot_label, records[4].file_label, records[4].matrix)]
+for pl1, fl1, m1, pl2, fl2, m2 in pairs:
     fig, ax = plt.subplots()
-    ax.set_title(f'log10 ratio of {l1} to {l2}')
+    ax.set_title(f'log10 ratio of {pl1} to {pl2}')
     ax.set_xlim(0, 20)
     ax.set_xticks(range(1, 21))
     ax.set_xticklabels(order, fontdict={'fontsize': 8})
@@ -194,17 +196,17 @@ for l1, m1, l2, m2 in pairs:
     ax.add_patch(Circle((22, 6.5), 2.5*scale, facecolor='white', edgecolor='black', clip_on=False))
     ax.text(23, 6.5, 'R < 1', size=8, va='center', clip_on=False)
 
-    plt.savefig(f'out/dotplot_ratio_{l1}-{l2}.png')
+    plt.savefig(f'out/dotplot_ratio_{fl1}-{fl2}.png')
     plt.close()
 
 # 5 DIFFERENCE DOT COMPARISONS
 scale = 0.06
-pairs = [(records[1][0], records[1][1], 'WAG', WAG_matrix),
-         (records[4][0], records[4][1], 'WAG', WAG_matrix),
-         (records[1][0], records[1][1], records[4][0], records[4][1])]
-for l1, m1, l2, m2 in pairs:
+pairs = [(records[1].plot_label, records[1].file_label, records[1].matrix, 'WAG', 'WAG', WAG_matrix),
+         (records[4].plot_label, records[4].file_label, records[4].matrix, 'WAG', 'WAG', WAG_matrix),
+         (records[1].plot_label, records[1].file_label, records[1].matrix, records[4].plot_label, records[4].file_label, records[4].matrix)]
+for pl1, fl1, m1, pl2, fl2, m2 in pairs:
     fig, ax = plt.subplots()
-    ax.set_title(f'Difference of {l1} and {l2}')
+    ax.set_title(f'Difference of {pl1} and {pl2}')
     ax.set_xlim(0, 20)
     ax.set_xticks(range(1, 21))
     ax.set_xticklabels(order, fontdict={'fontsize': 8})
@@ -239,18 +241,18 @@ for l1, m1, l2, m2 in pairs:
     ax.add_patch(Circle((22, 6.5), 4*scale, facecolor='white', edgecolor='black', clip_on=False))
     ax.text(23, 6.5, 'D < 0', size=8, va='center', clip_on=False)
 
-    plt.savefig(f'out/dotplot_diff_{l1}-{l2}.png')
+    plt.savefig(f'out/dotplot_diff_{fl1}-{fl2}.png')
     plt.close()
 
 # 6 FREQUENCIES
 width = 0.2
-bars = [(records[1][0], records[1][2], 'black'),
-        (records[4][0], records[4][2], 'grey'),
+bars = [(records[1].plot_label, records[1].freqs, 'black'),
+        (records[4].plot_label, records[4].freqs, 'grey'),
         ('WAG', WAG_freqs, 'white')]
 plt.figure(figsize=(8, 4))
-for i, (label, freqs, color) in enumerate(bars):
+for i, (plot_label, freqs, color) in enumerate(bars):
     dx = -len(bars)//2 + i + (1.5 if len(bars)%2 == 0 else 1)
-    plt.bar([x+width*dx for x in range(len(order))], freqs, label=label, facecolor=color, edgecolor='black', width=width)
+    plt.bar([x+width*dx for x in range(len(order))], freqs, label=plot_label, facecolor=color, edgecolor='black', width=width)
 plt.xticks(range(len(order)), order)
 plt.xlabel('Amino acid')
 plt.ylabel('Frequency')
