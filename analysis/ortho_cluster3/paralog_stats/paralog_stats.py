e67d214ff11f6ff85a04a179281b35f22a3ecb79
Marc Singleton <marcsingleton@berkeley.edu>
2022-04-13 16:28:16 -0700

    Added paralog_stats.py

---
+++ analysis/ortho_cluster3/paralog_stats/paralog_stats.py
@@ -0,0 +1,64 @@
+"""Statistics related to the addition of in-paralogs to clusters."""
+
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load OGs
+OGs1 = {}
+with open('../cluster4+_graph/out/4clique/clusters.tsv') as file:
+    file.readline()  # Skip header
+    for line in file:
+        _, OGid, _, edges = line.rstrip('\n').split('\t')
+        ppids = {node for edge in edges.split(',') for node in edge.split(':')}
+        OGs1[OGid] = ppids
+
+OGs2 = {}
+with open('../add_paralogs/out/clusters.tsv') as file:
+    file.readline()  # Skip header
+    for line in file:
+        _, OGid, _, edges = line.rstrip('\n').split('\t')
+        ppids = {node for edge in edges.split(',') for node in edge.split(':')}
+        OGs2[OGid] = ppids
+
+# Calculate stats
+rows = []
+for OGid, OG1 in OGs1.items():
+    OG2 = OGs2[OGid]
+    rows.append({'OGid': OGid, 'ppidnum1': len(OG1), 'ppidnum2': len(OG2), 'delta': len(OG2 - OG1)})
+df = pd.DataFrame(rows)
+
+if not os.path.exists('out/'):
+    os.mkdir('out/')
+
+# Make plots
+plt.scatter(df['ppidnum1'], df['ppidnum2'], s=5, alpha=0.2)
+plt.xlabel('Number of proteins in OG w/o in-paralogs')
+plt.ylabel('Number of proteins in OG w/ in-paralogs')
+plt.savefig('out/scatter_ppidnum2-ppidnum1.png')
+plt.close()
+
+counts = (df['delta'] == 0).value_counts()
+labels = [('w/ in-paralogs' if idx else 'w/o in-paralogs') + f'\n({value:,})' for idx, value in zip(counts.index, counts.values)]
+plt.pie(counts.values, labels=labels, labeldistance=1.4, textprops={'ha': 'center'})
+plt.title('OGs w/ and w/o in-paralogs')
+plt.savefig('out/pie_paralog.png')
+plt.close()
+
+counts = df.loc[df['delta'] != 0, 'delta'].value_counts()
+plt.bar(counts.index, counts.values, width=1)
+plt.xlabel('Number of in-paralogs')
+plt.ylabel('Number of OGs')
+plt.savefig('out/bar_OGnum-paralognum.png')
+plt.yscale('log')
+plt.savefig('out/bar_OGnum-paralognum_log.png')
+plt.close()
+
+"""
+DEPENDENCIES
+../add_paralogs/add_paralogs.py
+    ../add_paralogs/out/clusters.tsv
+../cluster4+_graph/cluster4+_graph.py
+    ../cluster4+_graph/out/4clique/clusters.tsv
+"""
