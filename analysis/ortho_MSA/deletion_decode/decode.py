80b5125a2b7f47240e64ebdd30a4d23d4a7e6c89
Marc Singleton <marcsingleton@berkeley.edu>
2022-07-27 16:44:24 -0700

    Updated deletion_hmm/ plot.py and decode.py to use new model

--- analysis/ortho_MSA/deletion_decode/decode.py
+++ analysis/ortho_MSA/deletion_decode/decode.py
@@ -1,4 +1,4 @@
-"""Decode long deletions of trimmed alignments."""
+"""Decode sequences into posterior probabilities."""
 
 import json
 import multiprocessing as mp
@@ -6,82 +6,93 @@ import os
 import re
 
 import homomorph
+import numpy as np
+import skbio
+from src.ortho_MSA import utils
 from src.utils import read_fasta
 
 
-class msaBernoulli:
-    def __init__(self, ps):
-        self.ps = ps
+def decode(OGid, model_json, tree_template):
+    # Load MSA
+    msa = []
+    for header, seq in read_fasta(f'../insertion_trim/out/{OGid}.afa'):
+        msa_ppid = re.search(ppid_regex, header).group(1)
+        msa_spid = re.search(spid_regex, header).group(1)
+        msa.append({'ppid': msa_ppid, 'spid': msa_spid, 'seq': seq})
 
-    def pmf(self, x):
-        p = self.ps[x[0]]
-        if x[1] == 1:
-            return p
-        else:
-            return 1 - p
-
-    def rvs(self, size=None):
-        # Required for HMM since it has a simulate method
-        # Simulations aren't used here, so it's an empty method
-        pass
-
-
-def decode(OGid, params):
-    # Load msa
-    msa = read_fasta(f'../insertion_trim/out/{OGid}.afa')
-
-    # Create Bernoulli sequence
-    ps = []
-    for j in range(len(msa[0][1])):
-        col = [1 if msa[i][1][j] in ['-', '.'] else 0 for i in range(len(msa))]
-        p = sum(col) / len(col)
-        ps.append(p)
-
-    # Instantiate model
-    e_dists_rv = {'0': msaBernoulli(ps), '1': msaBernoulli([params['e_param'] for _ in range(len(ps))])}
-    model = homomorph.HMM(params['t_dists'], e_dists_rv, params['start_dist'])
-
-    # Decode states
-    records = []
-    for header, seq in msa:
-        # Create emission sequence
-        emits = []
+    # Load tree and convert to vectors at tips
+    tree = tree_template.shear([record['spid'] for record in msa])
+    tips = {tip.name: tip for tip in tree.tips()}
+    tree.tip_dict = tips
+    for record in msa:
+        spid, seq = record['spid'], record['seq']
+        conditional = np.zeros((2, len(seq)))
         for j, sym in enumerate(seq):
             if sym in ['-', '.']:
-                emits.append((j, 1))
+                conditional[0, j] = 1
             else:
-                emits.append((j, 0))
+                conditional[1, j] = 1
+        tip = tips[spid]
+        tip.conditional = conditional
 
-        ppid = re.search(ppid_regex, header).group(1)
-        fbs = model.forward_backward(emits)
+    records = []
+    for record in msa:
+        spid, ppid = record['spid'], record['ppid']
+
+        # Instantiate model
+        e_dists_rv = {}
+        for s, e_dist in model_json['e_dists'].items():
+            if s == '1':
+                pi, q0, q1 = [e_dist[param] for param in ['pi', 'q0', 'q1']]
+                pmf = utils.get_tip_pmf(tree, spid, pi, q0, q1, 0, 0)
+                e_dists_rv[s] = utils.ArrayRV(pmf)
+            elif s == '2':
+                p = e_dist['p']
+                conditional = tree.tip_dict[spid].conditional[1]  # Second row is gaps=0, non-gaps=1
+                pmf = utils.get_bernoulli_pmf(conditional, p)
+                e_dists_rv[s] = utils.ArrayRV(pmf)
+        model = homomorph.HMM(model_json['t_dists'], e_dists_rv, model_json['start_dist'])
+
+        # Decode states and write
+        idx_seq = list(range(len(msa[0]['seq'])))  # Everything is pre-calculated, so emit_seq is the emit index
+        fbs = model.forward_backward(idx_seq)
         records.append((ppid, fbs))
 
-    # Write decoded states
+    state_set = sorted(model_json['t_dists'])
     with open(f'out/{OGid}.tsv', 'w') as file:
-        file.write('\t'.join(['ppid'] + states) + '\n')
+        file.write('ppid\t' + '\t'.join(state_set) + '\n')
         for ppid, fbs in records:
-            for fb in zip(*[fbs[state] for state in states]):
-                file.write(ppid + '\t' + '\t'.join([str(v) for v in fb]) + '\n')
+            for fb in zip(*[fbs[state] for state in state_set]):
+                file.write(f'{ppid}\t' + '\t'.join([str(v) for v in fb]) + '\n')
 
 
-ppid_regex = r'ppid=([A-Za-z0-9_]+)'
 num_processes = int(os.environ['SLURM_CPUS_ON_NODE'])
-states = ['0', '1']
+ppid_regex = r'ppid=([A-Za-z0-9_.]+)'
+spid_regex = r'spid=([a-z]+)'
+
+tree_template = skbio.read('../../ortho_tree/consensus_GTR2/out/NI.nwk', 'newick', skbio.TreeNode)
 
 if __name__ == '__main__':
+    # Load OGids
+    OGids = []
+    for path in os.listdir('../insertion_trim/out'):
+        if path.endswith('.afa'):
+            OGids.append(path)
+
     with open('../deletion_hmm/out/model.json') as file:
-        params = json.load(file)
+        model_json = json.load(file)
 
     if not os.path.exists('out/'):
         os.mkdir('out/')
 
     with mp.Pool(processes=num_processes) as pool:
-        args = [(path.removesuffix('.afa'), params) for path in os.listdir('../insertion_trim/out/') if path.endswith('.afa')]
-        pool.starmap(decode, args)
+        pool.starmap(decode, [(OGid, model_json, tree_template) for OGid in OGids])
 
 """
 DEPENDENCIES
-../insertion_trim/extract.py
+../../ortho_tree/consensus_GTR2/consensus_GTR2.py
+    ../../ortho_tree/consensus_GTR2/out/NI.nwk
+../insertion_trim/trim.py
     ../insertion_trim/out/*.afa
 ../deletion_hmm/fit.py
     ../deletion_hmm/out/model.json
--- analysis/ortho_MSA/deletion_hmm/plot.py
+++ analysis/ortho_MSA/deletion_hmm/plot.py
@@ -1,88 +1,205 @@
 """Plot example alignments segmented via posterior decoding."""
 
+import os
 import json
 import re
 
 import homomorph
 import matplotlib.pyplot as plt
-import src.draw as draw
+import numpy as np
+import skbio
+from matplotlib.lines import Line2D
+from src.ortho_MSA import utils
+from src.draw import plot_msa_data
 from src.utils import read_fasta
 
+ppid_regex = r'ppid=([A-Za-z0-9_.]+)'
+spid_regex = r'spid=([a-z]+)'
+state_set = ['1', '2']
+state_colors = ['C0', 'C1']
 
-class msaBernoulli:
-    def __init__(self, ps):
-        self.ps = ps
+tree_template = skbio.read('../../ortho_tree/consensus_GTR2/out/NI.nwk', 'newick', skbio.TreeNode)
+tree_order = skbio.read('../../ortho_tree/consensus_LG/out/100R_NI.nwk', 'newick', skbio.TreeNode)
+tip_order = {tip.name: i for i, tip in enumerate(tree_order.tips())}
 
-    def pmf(self, x):
-        p = self.ps[x[0]]
-        if x[1] == 1:
-            return p
-        else:
-            return 1 - p
-
-    def rvs(self, random_state=None):
-        # Required for HMM since it has a simulate method
-        # Simulations aren't used here, so it's an empty method
-        pass
-
-
-ppid_regex = r'ppid=([A-Za-z0-9_]+)'
-
-# Load model parameters
-with open('out/model.json') as file:
-    params = json.load(file)
-
-
-# Load OGids
-records = set()
+# Load labels
+OGid2labels = {}
+state_labels = set()
 with open('labels.tsv') as file:
     field_names = file.readline().rstrip('\n').split('\t')
     for line in file:
         fields = {key: value for key, value in zip(field_names, line.rstrip('\n').split('\t'))}
-        records.add((fields['OGid'], fields['ppid']))
+        OGid, ppid, start, stop, label = fields['OGid'], fields['ppid'], int(fields['start']), int(fields['stop']), fields['label']
+        state_labels.add(label)
+        try:
+            OGid2labels[(OGid, ppid)].append((start, stop, label))
+        except KeyError:
+            OGid2labels[(OGid, ppid)] = [(start, stop, label)]
+
+if set(state_set) != state_labels:
+    raise RuntimeError('state_labels is not equal to state_set')
+
+# Load history and model parameters
+with open('out/history.json') as file:
+    history = json.load(file)
+with open('out/model.json') as file:
+    model_json = json.load(file)
+
+# Plot loss curve
+xs = [record['iter_num'] for record in history]
+ys = [record['ll'] for record in history]
+plt.plot(xs, ys)
+plt.xlabel('Iteration')
+plt.ylabel('Conditional log-likelihood')
+plt.savefig('out/line_ll-iter.png')
+plt.close()
+
+# Plot model parameters
+params = ['pi', 'q0', 'q1']
+fig, axs = plt.subplots(len(params), 1)
+label, color = state_set[0], state_colors[0]
+for ax, param in zip(axs, params):
+    xs = [record['iter_num'] for record in history]
+    ys = [record['e_dists_norm'][label][param] for record in history]
+    ax.plot(xs, ys, label=label, color=color)
+    ax.set_ylabel(param)
+axs[-1].set_xlabel('Iteration')
+handles = [Line2D([], [], label=label, color=color) for label, color in zip(state_set[:1], state_colors[:1])]
+fig.legend(handles=handles, bbox_to_anchor=(0.875, 0.5), loc='center left')
+plt.subplots_adjust(right=0.875)
+plt.savefig('out/line_rate-iter.png')
+plt.close()
+
+param = 'p'
+fig, ax = plt.subplots()
+label, color = state_set[1], state_colors[1]
+xs = [record['iter_num'] for record in history]
+ys = [record['e_dists_norm'][label][param] for record in history]
+ax.plot(xs, ys, label=label, color=color)
+ax.set_ylabel(param)
+ax.set_xlabel('Iteration')
+handles = [Line2D([], [], label=label, color=color) for label, color in zip(state_set[1:], state_colors[1:])]
+fig.legend(handles=handles, bbox_to_anchor=(0.875, 0.5), loc='center left')
+plt.subplots_adjust(right=0.875)
+plt.savefig('out/line_p-iter.png')
+plt.close()
+
+fig, axs = plt.subplots(len(state_set), 1)
+for label, color in zip(state_set, state_colors):
+    for ax, param in zip(axs, state_set):
+        if param == label:
+            continue
+        xs = [record['iter_num'] for record in history]
+        ys = [record['t_dists_norm'][label][param] for record in history]
+        ax.plot(xs, ys, label=label, color=color)
+        ax.set_ylabel(param)
+for ax in axs:
+    ax.set_yscale('log')
+axs[-1].set_xlabel('Iteration')
+handles = [Line2D([], [], label=label, color=color) for label, color in zip(state_set, state_colors)]
+fig.legend(handles=handles, bbox_to_anchor=(0.875, 0.5), loc='center left')
+plt.subplots_adjust(left=0.15, right=0.875)
+plt.savefig('out/line_transition-iter.png')
+plt.close()
 
 # Plot alignments
-for OGid, ppid in records:
+if not os.path.exists('out/traces/'):
+    os.mkdir('out/traces/')
+
+for (OGid, ppid), labels in OGid2labels.items():
     # Load MSA
-    msa = read_fasta(f'../insertion_trim/out/{OGid}.afa')
-    seq = [seq for header, seq in msa if re.search(ppid_regex, header).group(1) == ppid][0]
+    msa, ppid2spid = [], {}
+    for header, seq in read_fasta(f'../realign_hmmer/out/mafft/{OGid}.afa'):
+        msa_ppid = re.search(ppid_regex, header).group(1)
+        msa_spid = re.search(spid_regex, header).group(1)
+        msa.append({'ppid': msa_ppid, 'spid': msa_spid, 'seq': seq})
+        ppid2spid[msa_ppid] = msa_spid
+    msa = sorted(msa, key=lambda x: tip_order[x['spid']])  # Re-order sequences
 
-    # Create Bernoulli sequence
-    ps = []
-    for j in range(len(msa[0][1])):
-        col = [1 if msa[i][1][j] in ['-', '.'] else 0 for i in range(len(msa))]
-        p = sum(col) / len(col)
-        ps.append(p)
-
-    # Create emission sequence
-    emits = []
-    for j, sym in enumerate(seq):
-        if sym in ['-', '.']:
-            emits.append((j, 1))
-        else:
-            emits.append((j, 0))
+    # Load tree and convert to vectors at tips
+    tree = tree_template.shear([record['spid'] for record in msa])
+    tips = {tip.name: tip for tip in tree.tips()}
+    tree.tip_dict = tips
+    for record in msa:
+        spid, seq = record['spid'], record['seq']
+        conditional = np.zeros((2, len(seq)))
+        for j, sym in enumerate(seq):
+            if sym in ['-', '.']:
+                conditional[0, j] = 1
+            else:
+                conditional[1, j] = 1
+        tip = tips[spid]
+        tip.conditional = conditional
 
     # Instantiate model
-    e_dists_rv = {'0': msaBernoulli(ps), '1': msaBernoulli([params['e_param'] for _ in range(len(ps))])}
-    model = homomorph.HMM(params['t_dists'], e_dists_rv, params['start_dist'])
+    e_dists_rv = {}
+    for s, e_dist in model_json['e_dists'].items():
+        if s == '1':
+            pi, q0, q1 = [e_dist[param] for param in ['pi', 'q0', 'q1']]
+            pmf = utils.get_tip_pmf(tree, ppid2spid[ppid], pi, q0, q1, 0, 0)
+            e_dists_rv[s] = utils.ArrayRV(pmf)
+        elif s == '2':
+            p = e_dist['p']
+            conditional = tree.tip_dict[ppid2spid[ppid]].conditional[1]  # Second row is gaps=0, non-gaps=1
+            pmf = utils.get_bernoulli_pmf(conditional, p)
+            e_dists_rv[s] = utils.ArrayRV(pmf)
+    model = homomorph.HMM(model_json['t_dists'], e_dists_rv, model_json['start_dist'])
 
-    # Decode states and plot
-    fbs = model.forward_backward(emits)
-    draw.plot_msa_data([seq for _, seq in msa], fbs['1'],
-                       msa_labels=[ppid if ppid in header else '' for header, _ in msa], msa_labelsize=4,
-                       figsize=(15, 6))
-    plt.savefig(f'out/{OGid}_{ppid}_wide.png', bbox_inches='tight')
+    # Make plotting parameters
+    msa_labels = [record['ppid'] if record['ppid'] == ppid else '' for record in msa]
+
+    kwargs_wide = {'figsize': (15, 6), 'height_ratio': 0.5, 'hspace': 0.2,
+                   'msa_labels': msa_labels, 'msa_labelsize': 4,
+                   'data_max': 1.1, 'data_min': -0.1, 'data_labels': state_set, 'data_colors': state_colors,
+                   'msa_legend': True, 'legend_kwargs': {'bbox_to_anchor': (0.945, 0.5), 'loc': 'center left', 'fontsize': 8,
+                                                         'handletextpad': 0.5, 'markerscale': 1.25, 'handlelength': 1}}
+    adjust_wide = {'left': 0.04, 'bottom': 0.01, 'right': 0.94, 'top': 0.99}
+
+    kwargs_tall = {'figsize': (8, 8), 'height_ratio': 0.5, 'hspace': 0.2,
+                   'msa_labels': msa_labels, 'msa_labelsize': 4,
+                   'data_max': 1.1, 'data_min': -0.1, 'data_labels': state_set, 'data_colors': state_colors,
+                   'msa_legend': True, 'legend_kwargs': {'bbox_to_anchor': (0.90, 0.5), 'loc': 'center left', 'fontsize': 8,
+                                                         'handletextpad': 0.5, 'markerscale': 1.25, 'handlelength': 1}}
+    adjust_tall = {'left': 0.06, 'bottom': 0.01, 'right': 0.89, 'top': 0.99}
+
+    # Plot labels
+    lines = {label: np.zeros(len(msa[0]['seq'])) for label in state_set}
+    for start, stop, label in labels:
+        lines[label][start:stop] = 1
+    data = [lines[label] for label in state_set]
+
+    plot_msa_data([record['seq'] for record in msa], data, **kwargs_wide)
+    plt.subplots_adjust(**adjust_wide)
+    plt.savefig(f'out/traces/{OGid}-{ppid}_wide_labels.png')
     plt.close()
 
-    draw.plot_msa_data([seq for _, seq in msa], fbs['1'],
-                       msa_labels=[ppid if ppid in header else '' for header, _ in msa], msa_labelsize=4,
-                       figsize=(8, 8))
-    plt.savefig(f'out/{OGid}_{ppid}_tall.png', bbox_inches='tight')
+    plot_msa_data([record['seq'] for record in msa], data, **kwargs_tall)
+    plt.subplots_adjust(**adjust_tall)
+    plt.savefig(f'out/traces/{OGid}-{ppid}_tall_labels.png')
+    plt.close()
+
+    # Decode states and plot
+    idx_seq = list(range(len(msa[0]['seq'])))  # Everything is pre-calculated, so emit_seq is the emit index
+    fbs = model.forward_backward(idx_seq)
+    data = [fbs[label] for label in state_set]
+
+    plot_msa_data([record['seq'] for record in msa], data, **kwargs_wide)
+    plt.subplots_adjust(**adjust_wide)
+    plt.savefig(f'out/traces/{OGid}-{ppid}_wide.png')
+    plt.close()
+
+    plot_msa_data([record['seq'] for record in msa], data, **kwargs_tall)
+    plt.subplots_adjust(**adjust_tall)
+    plt.savefig(f'out/traces/{OGid}-{ppid}_tall.png')
     plt.close()
 
 """
 DEPENDENCIES
-../insertion_trim/extract.py
+../../ortho_tree/consensus_GTR2/consensus_GTR2.py
+    ../../ortho_tree/consensus_GTR2/out/NI.nwk
+../../ortho_tree/consensus_LG/consensus_LG.py
+    ../../ortho_tree/consensus_LG/out/100R_NI.nwk
+../insertion_trim/trim.py
     ../trim_extract/out/*.afa
 ./fit.py
     ./out/model.json
