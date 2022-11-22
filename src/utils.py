3086e3af1e36c8f8037a721e87da375cb4a45136
Marc Singleton <marcsingleton@berkeley.edu>
2022-11-22 11:41:43 -0800

    Fixed read_fasta docs stating header > is removed

--- src/utils.py
+++ src/utils.py
@@ -75,7 +75,7 @@ def read_fasta(path):
     Returns
     -------
     fasta: list of tuples of (header, seq)
-        The first element is the header line without the >, and the second
+        The first element is the header line with the >, and the second
         element is the corresponding sequence.
     """
     fasta = []
