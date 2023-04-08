5c242d53b67fc550c918274a56ca42e2e67bf9be
Marc Singleton <marcsingleton@berkeley.edu>
2023-04-07 22:29:01 -0700

    Added linewidth and removed alpha arguments from plot_msa_data

--- src/draw.py
+++ src/draw.py
@@ -131,7 +131,7 @@ def plot_msa_data(msa, data, *,
                   height_ratio=1, hspace=0.25, sym_length=7, sym_height=7,
                   fig=None, figsize=(15, 6), left=0.05, right=0.95, top=0.95, bottom=0.05, anchor=(0.5, 0.5),
                   data_min=None, data_max=None,
-                  data_labels=None, data_colors=None, data_alphas=None,
+                  data_labels=None, data_linewidths=None, data_colors=None,
                   msa_legend=False, legend_kwargs=None,
                   block_columns=None, sym2color=None, gap2color=None):
     """Plot MSA with associated positional data as matplotlib figure.
@@ -189,10 +189,11 @@ def plot_msa_data(msa, data, *,
     data_labels: list of strings
         Labels for data series. If not None, will draw legend. Length must
         match the number of series in data.
+    data_linewidths: float, int, or list of float or int
+        Line widths for data series. If list, length must match number of
+        series in data.
     data_colors: list of colors
         Colors for data series. Length must match number of series in data.
-    data_alphas: list of float
-        Alphas for data series. Length must match number of series in data.
     msa_legend: bool
         True if MSA legend is drawn.
     legend_kwargs: dict
@@ -250,15 +251,17 @@ def plot_msa_data(msa, data, *,
         data_max += 0.5
     if data_labels is not None and len(data_labels) != len(data):
         raise ValueError('len(data_labels) does not match len(data)')
+    if data_linewidths is None:
+        data_linewidths = [None for _ in range(len(data))]
+    elif isinstance(data_linewidths, float) or isinstance(data_linewidths, int):
+        data_linewidths = [data_linewidths for _ in range(len(data))]
+    elif len(data_linewidths) != len(data):
+        raise ValueError('len(data_colors) does not match len(data)')
     if data_colors is None:
         color_cycle = rcParams['axes.prop_cycle'].by_key()['color']
         data_colors = [color_cycle[i % len(color_cycle)] for i in range(len(data))]
     elif len(data_colors) != len(data):
         raise ValueError('len(data_colors) does not match len(data)')
-    if data_alphas is None:
-        data_alphas = [1 for _ in range(len(data))]
-    elif len(data_alphas) != len(data):
-        raise ValueError('len(data_alphas) does not match len(data)')
     block_number = ceil(msa_columns / block_columns)
     block_rows = len(msa)
 
@@ -290,9 +293,9 @@ def plot_msa_data(msa, data, *,
         for spine in ['left', 'right', 'top', 'bottom']:
             msa_ax.spines[spine].set_visible(False)
 
-        for series, color, alpha in zip(data, data_colors, data_alphas):
+        for series, linewidth, color in zip(data, data_linewidths, data_colors):
             data_ax.plot(range(x_left, x_right), series[i * block_columns:i * block_columns + block.shape[1] // sym_length],
-                         color=color, alpha=alpha)
+                         linewidth=linewidth, color=color)
         data_ax.set_ylim(data_min, data_max)
         data_ax.tick_params(axis='y', labelsize=y_labelsize)
         data_ax.tick_params(axis='x', labelsize=x_labelsize)
